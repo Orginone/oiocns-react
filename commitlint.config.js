2b19121e031c5c1af95eb01ac0c8eaa830ed6e5e
SEN <changsen25@163.com>
2022-11-15 11:37:59 +0800

    test git

--- commitlint.config.js
+++ commitlint.config.js
@@ -27,28 +27,25 @@ module.exports = {
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 108],
     'subject-empty': [2, 'never'],
-    'type-empty': [2, 'never'],
+    'type-empty': [2, 'always'],
     'type-enum': [
       2,
       'always',
       [
-        'feat',
-        'fix',
-        'add',
-        'perf',
-        'style',
-        'docs',
-        'test',
-        'refactor',
-        'build',
-        'ci',
-        'chore',
-        'revert',
-        'wip',
+        'feat', // 新功能 feature
+        'add', // 新增功能
+        'fix', // 修复 bug
+        'docs', // 文档注释
+        'style', // 代码格式(不影响代码运行的变动)
+        'refactor', // 重构(既不增加新功能，也不是修复bug)
+        'perf', // 性能优化
+        'test', // 增加测试
+        'chore', // 构建过程或辅助工具的变动
+        'revert', // 回退
+        'build', // 打包
         'workflow',
         'types',
         'release',
-        'other',
       ],
     ],
   },
