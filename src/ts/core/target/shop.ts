2f9a3a2fbe9eff50aa45e30fc8069046ee80b61c
SEN <changsen25@163.com>
2022-11-16 17:55:26 +0800

    add: shop

--- src/ts/core/target/shop.ts
+++ src/ts/core/target/shop.ts
@@ -1,8 +1,22 @@
-
 import { kernel, model, common, schema } from '../../base';
-import BaseTarget from './base'
+import BaseTarget from './base';
 export default class Shop extends BaseTarget {
-    constructor(target: schema.XTarget) {
-        super(target);
-      }
+  constructor(target: schema.XTarget) {
+    super(target);
+  }
+
+  // 重命名
+  RenameItem() {}
+  // 拷贝副本
+  copyItem() {}
+  // 复制链接
+  copyLink() {}
+  // 移动到
+  moveTo() {}
+  // 收藏
+  favorite() {}
+  // 删除
+  deleteItem() {}
+  //获取列表
+  getapps() {}
 }
