7f83e37d83fb5f4a79188919f488034a52a1b0ce
wangbo <wb03885132@163.com>
2022-11-16 11:57:18 +0800

    去除无用代码

--- src/pages/Setting/Flow/index.tsx
+++ src/pages/Setting/Flow/index.tsx
@@ -1,11 +1,6 @@
 import { Card } from 'antd';
 import React from 'react';
 import cls from './index.module.less';
-import RootNode from '@/bizcomponents/Flow/Process/RootNode';
-import ApprovalNode from '@/bizcomponents/Flow/Process/ApprovalNode';
-import CcNode from '@/bizcomponents/Flow/Process/CcNode';
-import ConcurrentNode from '@/bizcomponents/Flow/Process/ConcurrentNode';
-import ConditionNode from '@/bizcomponents/Flow/Process/ConditionNode';
 import ProcessDesign from '@/bizcomponents/Flow/ProcessDesign';
 /**
  * 流程设置
