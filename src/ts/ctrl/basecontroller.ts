72a39c2e33765bb61f5448b319b2efd3b79fa993
ouyangjun <ouyangjun@apexsoft.com.cn>
2022-11-17 11:52:41 +0800

    提交

--- src/ts/ctrl/basecontroller.ts
+++ src/ts/ctrl/basecontroller.ts
@@ -1,9 +1,17 @@
+import Provider from '@/ts/core/provider';
+import Person from '../core/target/person';
+
 /**
  * 控制器的基类
  */
 abstract class BaseController {
 
+    // 获取登录的用户
+    public static get getPerson(): Person {
+        return Provider.getPerson;
+    }
+
+
 }
 
-export default BaseController; 
-  
+export default BaseController;
