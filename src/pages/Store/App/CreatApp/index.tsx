b8cbc2b47edefbcf07484f0c0c3ee747240f6498
cclu <lkrnchocolateaaron@gmail.com>
2022-11-17 16:10:26 +0800

    fix: 左侧菜单添加自定义判断逻辑

--- src/pages/Store/App/CreatApp/index.tsx
+++ src/pages/Store/App/CreatApp/index.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
-// import cls from './index.module.less';
+import cls from './index.module.less';
 import SchemaForm from '@/components/SchemaForm';
 import { columns, DataItem } from './config';
 import { Form, Card } from 'antd';
-interface indexType {
-  [key: string]: any;
-}
-const CreatApp: React.FC<indexType> = () => {
+import { IconFont } from '@/components/IconFont';
+import { RouteComponentProps } from 'react-router-dom';
+
+const CreatApp: React.FC<RouteComponentProps> = (props) => {
+  const { history } = props;
   const [showCreateModal, setShowCreateModal] = useState<boolean>(false); // 是否显示创建应用窗口
   const [createAppForm] = Form.useForm<Record<string, any>>();
   // 提交表单数据
@@ -18,7 +19,16 @@ const CreatApp: React.FC<indexType> = () => {
     // debugger;
   }, []);
   return (
-    <Card>
+    <Card
+      title={
+        <IconFont
+          type="icon-jiantou-left"
+          className={cls.RouterBackBtn}
+          onClick={() => {
+            history.goBack();
+          }}
+        />
+      }>
       <SchemaForm<DataItem>
         form={createAppForm}
         layoutType="Form"
