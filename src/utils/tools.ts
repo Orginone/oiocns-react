8ad1420b1ab96ab6bec3001c1ef93fd0d783d2d5
cclu <lkrnchocolateaaron@gmail.com>
2022-11-17 10:56:58 +0800

    fix: 前后端分页转化方法迁移至utils-tool

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -10,6 +10,22 @@ const debounce = (fun: any, delay?: number) => {
   };
 };
 
+/**
+ * @desc: 处理 翻页参数问题
+ * @param {T} params
+ * @return {*}
+ */
+const _resetParams = (params: any) => {
+  const { page, pageSize, ...rest } = params;
+  const num = (page - 1) * pageSize;
+
+  return {
+    offset: num >= 0 ? num : 0,
+    limit: pageSize || 20,
+    ...rest,
+  };
+};
+
 // m--n 之间的数字
 const renderNum = (m: number, n: number) => {
   return Math.floor(Math.random() * (n + 1 - m) + m);
