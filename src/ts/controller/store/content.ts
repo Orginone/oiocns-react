062e92d31aa070fb372fe5f8ea435bbd3f6467ca
SEN <changsen25@163.com>
2022-11-18 18:05:28 +0800

    feat: 调用商店实例 请求商店下的应用

--- src/ts/controller/store/content.ts
+++ src/ts/controller/store/content.ts
@@ -1,3 +1,5 @@
+import { XMarket, XMerchandise } from '@/ts/base/schema';
+import AppStore from '@/ts/core/market/appstore';
 import StoreSiderbar from './sidebar';
 /**
  * @desc: 仓库 展示区 控件
@@ -7,23 +9,27 @@ import StoreSiderbar from './sidebar';
 class StoreContent {
   currentMenu = 'Public'; //当前展示 菜单
   curObject = {};
+  public_store: XMerchandise[] = [];
   // curMarket = storeClassify._curMarket;
   storeClassify: any;
+  public _curMarket: AppStore | undefined = new AppStore({
+    id: '358266491960954880',
+  } as XMarket); // 当前商店信息
   // 存储 数据
   // 最近使用应用
   // 我的应用列表
   // 商品列表
   constructor() {
-    setTimeout(() => {
-      console.log('內容區域', StoreSiderbar);
-    }, 10);
+    this.getShowData();
   }
 
   /**
    * async getMarket
    */
-  public async getMarket() {
+  public async getShowData() {
     // this.storeClassify = await import('./classify');
+
+    console.log('shuju1', '对对对');
   }
   /**
    * 切换侧边栏 触发 展示数据变化
@@ -42,7 +48,18 @@ class StoreContent {
    * @desc: 获取主体展示数据 --根据currentMenu 判断请求 展示内容
    * @return {*}
    */
-  public getMainData() {}
+  public async getMainData() {
+    const res = await this._curMarket!.getMerchandise({
+      offset: 0,
+      limit: 10,
+      filter: '',
+    });
+    const { success, data = { result: [] } } = res;
+    if (success) {
+      const { result = [] } = data;
+      this.public_store = [...result];
+    }
+  }
 }
 const storeContent = new StoreContent();
 
