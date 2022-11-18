1d214ca9b13a382b8227480e60a3112260fcef11
SEN <changsen25@163.com>
2022-11-18 11:27:42 +0800

    fix: 格式化检测机制

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
   ],
   plugins: ['simple-import-sort', 'prettier'],
   rules: {
+    'no-debugger': 1,
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/consistent-type-assertions': 'off', //添加这行。
@@ -33,19 +34,27 @@ module.exports = {
     // 'jsx-a11y/no-static-element-interactions': ['off'],
     // 'jsx-a11y/click-events-have-key-events': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'no-use-before-define': 2,
+    'no-use-before-define': 'off',
     // 'simple-import-sort/imports': 'warn', // 引入顺序排序功能
     'simple-import-sort/exports': 'error', // 导出顺序排序
-
     //解决ts中，本身是可以有空的构造函数的误判问题 START
     'no-useless-constructor': 'off',
     // '@typescript-eslint/no-useless-constructor': 'error',
     'no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    // 'no-unused-vars': [
+    //   'warn',
+    //   {
+    //     vars: 'all',
+    //     args: 'after-used',
+    //     ignoreRestSiblings: true,
+    //     varsIgnorePattern: 'createElement',
+    //   },
+    // ],
     '@typescript-eslint/no-unused-vars': ['off'],
     'no-empty-function': 'off',
     //解决导出类型时，no-undef报错问题
     'no-undef': 'off',
     'import/prefer-default-export': 'off',
-    'no-import-assign': 2,
+    'no-import-assign': 'off',
   },
 };
