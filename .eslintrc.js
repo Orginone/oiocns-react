29f037ce9a6b6d03612a8cfd08e9f47e15266366
SEN <changsen25@163.com>
2022-11-18 10:53:04 +0800

    fix: git

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,23 +28,24 @@ module.exports = {
   rules: {
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/consistent-type-assertions': 'off', //添加这行。
     // 'jsx-a11y/accessible-emoji': 'off',
     // 'jsx-a11y/no-static-element-interactions': ['off'],
     // 'jsx-a11y/click-events-have-key-events': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
+    'no-use-before-define': 2,
     // 'simple-import-sort/imports': 'warn', // 引入顺序排序功能
     'simple-import-sort/exports': 'error', // 导出顺序排序
 
     //解决ts中，本身是可以有空的构造函数的误判问题 START
     'no-useless-constructor': 'off',
     // '@typescript-eslint/no-useless-constructor': 'error',
-    'no-unused-vars': ['error', { varsIgnorePattern: '.*', args: 'none' }],
-    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    '@typescript-eslint/no-unused-vars': ['off'],
     'no-empty-function': 'off',
     //解决导出类型时，no-undef报错问题
     'no-undef': 'off',
     'import/prefer-default-export': 'off',
-    'no-import-assign': 'off',
+    'no-import-assign': 2,
   },
 };
