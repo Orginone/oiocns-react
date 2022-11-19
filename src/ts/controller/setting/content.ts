8ab6e2b3e0cc9f873d08e79c414e053d8f126d25
zhangqiang <1196217890@qq.com>
2022-11-19 11:01:39 +0800

    add:合并了dev

---
+++ src/ts/controller/setting/content.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: zhangqiang 1196217890@qq.com
+ * @Date: 2022-11-19 10:49:44
+ * @LastEditors: zhangqiang 1196217890@qq.com
+ * @LastEditTime: 2022-11-19 10:52:58
+ * @FilePath: /oiocns-react/src/ts/controller/setting/content.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+class SettingContent  { 
+
+}
+
+const settingContent = new SettingContent();
+
+export default settingContent;
