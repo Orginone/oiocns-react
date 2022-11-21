c78b918b2e0b6c9af805c9cfa1a8ff071b687f8d
zhangqiang <1196217890@qq.com>
2022-11-21 10:57:01 +0800

    test:daily

--- src/pages/Setting/Dept/components/EditCustomModal/index.tsx
+++ src/pages/Setting/Dept/components/EditCustomModal/index.tsx
@@ -2,7 +2,7 @@
  * @Author: zhangqiang 1196217890@qq.com
  * @Date: 2022-11-14 16:43:05
  * @LastEditors: zhangqiang 1196217890@qq.com
- * @LastEditTime: 2022-11-19 17:21:02
+ * @LastEditTime: 2022-11-21 09:51:54
  * @FilePath: /oiocns-react/src/pages/Setting/Dept/components/EditCustomModal/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -27,11 +27,7 @@ const EditCustomModal = (props: Iprops) => {
   const { open, title, onOk, handleOk, handleCancel } = props;
   const [form] = Form.useForm();
   useEffect(() => {
-    form.setFieldsValue({
-      name: '顶顶保安公司',
-      code: '91310000MA1H3NAH64',
-      describe: '顶顶顶顶顶顶顶',
-    });
+   
   }, []);
   
   return (
