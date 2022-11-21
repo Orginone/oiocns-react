f93f188bf1bca55237f469531a7386522892cee1
Captain842 <1072144700@qq.com>
2022-11-21 15:04:05 +0800

    fix: 工作空间查询、切换

--- src/ts/core/provider.ts
+++ src/ts/core/provider.ts
@@ -1,5 +1,5 @@
-import { SpaceType } from '@/store/type';
 import { kernel, model } from '../base';
+import MarketActionTarget from './target/mbase';
 import Person from './target/person';
 
 /**
@@ -7,26 +7,57 @@ import Person from './target/person';
  */
 export default class Provider {
   private static person: Person;
-  private static _workSpace: SpaceType;
+  private static _workSpace: MarketActionTarget | undefined;
 
   public static get userId() {
-    return Provider.person.target.id;
+    return this.person.target.id;
+  }
+
+  public static async getAllWorkSpaces(): Promise<{ id: string; name: string }[]> {
+    var workSpaces = [];
+    if (this.person != null) {
+      workSpaces.push({ id: this.person.target.id, name: '个人空间' });
+      const companys = await this.person.getJoinedCompanys();
+      companys.forEach((element) => {
+        workSpaces.push({ id: element.target.id, name: element.target.name });
+      });
+    }
+    return workSpaces;
   }
 
   /**
-   * 获取工作空间
-   * @returns 工作空间
+   * 获取当前工作空间
+   * @returns 工作当前空间
    */
-  public static getWorkSpace(): SpaceType {
-    return Provider._workSpace;
+  public static async getWorkSpace(): Promise<MarketActionTarget | undefined> {
+    if (this._workSpace == null) {
+      var id = sessionStorage.getItem('_workSpaceId') + '';
+      if (this.person.target.id == id) {
+        return this.person;
+      } else {
+        const companys = await this.person.getJoinedCompanys();
+        return companys.find((company) => {
+          return company.target.id == id;
+        });
+      }
+    }
+    return this._workSpace;
   }
 
   /**
    * 切换工作空间
    * @param workSpace
    */
-  public static setWorkSpace(workSpace: SpaceType) {
-    Provider._workSpace = workSpace;
+  public static async setWorkSpace(id: string) {
+    sessionStorage.setItem('_workSpaceId', id);
+    if (this.person.target.id == id) {
+      this._workSpace = this.person;
+    } else {
+      const companys = await this.person.getJoinedCompanys();
+      this._workSpace = companys.find((company) => {
+        return company.target.id == id;
+      });
+    }
   }
 
   /**
@@ -34,7 +65,7 @@ export default class Provider {
    * @returns
    */
   public static isUserSpace(): boolean {
-    return Provider._workSpace.id == Provider.person.target.id;
+    return this._workSpace?.target.id == this.person.target.id;
   }
 
   public static get getPerson(): Person {
@@ -55,7 +86,7 @@ export default class Provider {
     let res = await kernel.login(account, password);
     if (res.success) {
       this.person = new Person(res.data.person);
-      this._workSpace = { id: this.person.target.id, name: '个人空间' };
+      this.setWorkSpace(this.person.target.id);
       sessionStorage.setItem('_loginPerson', JSON.stringify(res.data.person));
     }
     return res;
