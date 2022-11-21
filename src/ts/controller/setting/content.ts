789042d62c171c67d3de8b62452222fa6225bd74
zhangqiang <1196217890@qq.com>
2022-11-21 15:15:02 +0800

    add:临时提交

--- src/ts/controller/setting/content.ts
+++ src/ts/controller/setting/content.ts
@@ -2,15 +2,13 @@
  * @Author: zhangqiang 1196217890@qq.com
  * @Date: 2022-11-19 10:49:44
  * @LastEditors: zhangqiang 1196217890@qq.com
- * @LastEditTime: 2022-11-19 11:07:39
+ * @LastEditTime: 2022-11-21 14:41:15
  * @FilePath: /oiocns-react/src/ts/controller/setting/content.ts
  * @Description: 控制器 实例化要调用的接口基类 提供ui层数据
  */
-
-class SettingContent  { 
+import { LoadChats } from '../../core/chat';
+class SettingController  { 
 
 }
 
-const settingContent = new SettingContent();
-
-export default settingContent;
+export const settingCtrl = new SettingController(await LoadChats());
