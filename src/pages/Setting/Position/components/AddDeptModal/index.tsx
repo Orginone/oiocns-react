e4a54e00d2254d31852cd43cd0250dd4ddcb36fc
ouyangjun <ouyangjun@apexsoft.com.cn>
2022-11-28 20:41:29 +0800

    feat: 新增岗位编辑界面

---
+++ src/pages/Setting/Position/components/AddDeptModal/index.tsx
@@ -0,0 +1,150 @@
+/**
+ * 岗位设置弹窗
+ * */
+import React, { useState } from 'react';
+import { Modal, Card, Row, Col, Checkbox, Radio, Button, message } from 'antd';
+import SearchInput from '@/components/SearchInput';
+import cls from './index.module.less';
+import { DownOutlined, UpOutlined } from '@ant-design/icons';
+import { deptList, deptArrList } from './mock';
+import { deepClone } from '@/ts/base/common';
+import { initDatatype } from '@/ts/core/setting/isetting';
+
+interface Iprops {
+  title: string;
+  open: boolean;
+  onOk: (checkJob: initDatatype, checkUser: initDatatype[]) => void;
+  handleOk: () => void;
+}
+
+const AddDeptModal = (props: Iprops) => {
+  const { title, open, onOk, handleOk } = props;
+  let [openIdArr, setOpenIDArr] = useState<Array<string>>([]);
+  const [checkUser, setCheckUser] = useState<initDatatype[]>([]);
+  const [checkJob, setCheckJob] = useState<{ id?: string; name?: string }>({});
+  // 点击展开收起
+  const toggle = (selectId: string) => {
+    if (openIdArr.includes(selectId)) {
+      const newArr = openIdArr.filter((item) => item !== selectId);
+      setOpenIDArr(newArr);
+    } else {
+      setOpenIDArr([...openIdArr, selectId]);
+    }
+  };
+  const onChange = (val: any, id: string, name: string) => {
+    let currentValue;
+    if (val.target.checked) {
+      currentValue = deepClone(checkUser);
+      currentValue.push({ checked: val.target.checked, id, name });
+    } else {
+      currentValue = checkUser.filter((item) => {
+        return !(item.id === id);
+      });
+    }
+    setCheckUser(currentValue);
+  };
+
+  const onRadioChange = (_: any, id: string, name: string) => {
+    setCheckJob(Object.assign({}, { id: id, name: name }));
+  };
+
+  const cardLeft = (
+    <Card>
+      <SearchInput placeholder="请输入姓名、账号" onChange={onChange} />
+      <div className={cls[`cardleft-dept-overflow`]}>
+        {deptList.map((item): any => {
+          return (
+            <div key={item.id} className={cls[`cardleft-dept`]}>
+              <div
+                className={cls[`cardleft-dept-title`]}
+                onClick={() => {
+                  toggle(item.id);
+                }}>
+                <span className={cls[`cardleft-dept-title-name`]}>{item.name}</span>
+                <span className={cls[`cardleft-dept-title-icon`]}>
+                  {openIdArr.includes(item.id) ? <UpOutlined /> : <DownOutlined />}
+                </span>
+              </div>
+              {openIdArr.includes(item.id) ? (
+                <div className={cls[`cardleft-item`]}>
+                  {item.children.map((child) => {
+                    return (
+                      <div className={cls[`cardleft-item-children`]} key={child.id}>
+                        <Checkbox onChange={(e) => onChange(e, child.id, child.name)}>
+                          {child.name}
+                        </Checkbox>
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : (
+                ''
+              )}
+            </div>
+          );
+        })}
+      </div>
+      <Row justify="end">
+        <span>已选{3}位用户</span>
+      </Row>
+    </Card>
+  );
+  const cardRight = (
+    <Card>
+      <SearchInput placeholder="请输入岗位" onChange={onChange} />
+      <div className={cls[`cardright-dept-voerflow`]}>
+        {deptArrList.map((item) => {
+          return (
+            <div key={item.id} className={cls[`cardright-dept`]}>
+              <span className={cls[`cardright-dept-title-name`]}>{item.name}</span>
+              <div className={cls[`cardright-item`]}>
+                <Radio.Group value={checkJob.id || ''}>
+                  {item.children.map((child) => {
+                    return (
+                      <div key={child.id} className={cls[`cardright-item-children`]}>
+                        <Radio
+                          onChange={(e) => {
+                            onRadioChange(e.target.checked, child.id, child.name);
+                          }}
+                          value={child.id}>
+                          {child.name}
+                        </Radio>
+                      </div>
+                    );
+                  })}
+                </Radio.Group>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </Card>
+  );
+  const addmodal = (
+    <Modal
+      title={title}
+      open={open}
+      onCancel={() => handleOk()}
+      getContainer={false}
+      width={600}
+      footer={null}>
+      <div className="site-card-wrapper">
+        <Row gutter={24}>
+          <Col span={12}>{cardLeft}</Col>
+          <Col span={12}>{cardRight}</Col>
+        </Row>
+      </div>
+      <Row justify="end">
+        <Button
+          type="primary"
+          onClick={() => {
+            onOk(checkJob, checkUser);
+          }}>
+          完成
+        </Button>
+      </Row>
+    </Modal>
+  );
+  return <div className={cls[`add-person-modal`]}>{addmodal}</div>;
+};
+export default AddDeptModal;
---
+++ src/pages/Setting/Position/components/EditCustomModal/index.tsx
@@ -0,0 +1,126 @@
+/*
+ * @Author: zhangqiang 1196217890@qq.com
+ * @Date: 2022-11-14 16:43:05
+ * @LastEditors: zhangqiang 1196217890@qq.com
+ * @LastEditTime: 2022-11-21 09:51:54
+ * @FilePath: /oiocns-react/src/pages/Setting/Dept/components/EditCustomModal/index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React, { useEffect } from 'react';
+import { Modal, Form, Input, Row, Col, Space, Button, message } from 'antd';
+import cls from './index.module.less';
+import UploadAvatar from '../UploadAvatar';
+import settingController from '@/ts/controller/setting';
+/* 
+  编辑
+*/
+interface Iprops {
+  title: string;
+  open: boolean;
+  onOk: () => void;
+  handleOk: () => void;
+  handleCancel: () => void;
+  selectId?: string;
+}
+
+const { TextArea } = Input;
+
+const EditCustomModal = (props: Iprops) => {
+  const { open, title, onOk, handleOk, handleCancel, selectId } = props;
+  const [form] = Form.useForm();
+  useEffect(() => {}, []);
+
+  return (
+    <div className={cls['edit-custom-modal']}>
+      <Modal
+        title={title}
+        open={open}
+        onOk={handleOk}
+        onCancel={() => handleCancel()}
+        getContainer={false}
+        footer={null}>
+        <Form form={form} layout="vertical">
+          <Form.Item label="" name="avatar" noStyle>
+            <UploadAvatar />
+          </Form.Item>
+
+          <Row>
+            <Col span={12}>
+              <Form.Item
+                name="name"
+                label="单位名称"
+                rules={[{ required: true, message: '请输入单位名称!' }]}>
+                <Input placeholder="请输入单位名称" />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name="code"
+                label="单位编号"
+                rules={[{ required: true, message: '请输入单位编号!' }]}>
+                <Input placeholder="请输入单位编号" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <Form.Item
+                name="teamName"
+                label="我的岗位"
+                rules={[{ required: true, message: '请输入岗位名称!' }]}>
+                <Input placeholder="请输入岗位名称" />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name="teamCode"
+                label="团队编码"
+                rules={[{ required: true, message: '请输入团编码' }]}>
+                <Input placeholder="请输入团队编码" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <Form.Item
+                name="remark"
+                label="描述"
+                rules={[{ required: true, message: '请输入单位描述!' }]}>
+                <TextArea
+                  // value={value}
+                  // onChange={(e) => setValue(e.target.value)}
+                  placeholder="请输入单位描述"
+                  autoSize={{ minRows: 1, maxRows: 1 }}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row justify="end">
+            <Space>
+              <Button
+                type="primary"
+                onClick={async () => {
+                  const value = await form.validateFields();
+                  value.parentId = selectId;
+                  if (value) {
+                    const curentValue = await settingController.createDepartment(value);
+                    if (!curentValue.success) {
+                      message.error(curentValue.msg);
+                    } else {
+                      message.success('添加成功');
+                      settingController.trigger('updateDeptTree');
+                      handleOk();
+                    }
+                  }
+                }}>
+                完成
+              </Button>
+            </Space>
+          </Row>
+        </Form>
+      </Modal>
+    </div>
+  );
+};
+
+export default EditCustomModal;
