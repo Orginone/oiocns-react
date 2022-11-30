3272f04c075fe8754246421810919a98bc2ca3bd
SEN <changsen25@163.com>
2022-11-30 16:39:37 +0800

    fix: tree组件右键

--- src/components/CustomTreeComp/index.tsx
+++ src/components/CustomTreeComp/index.tsx
@@ -1,6 +1,6 @@
-import { EllipsisOutlined, PlusOutlined, SearchOutlined } from '@ant-design/icons';
+import { EllipsisOutlined, SearchOutlined } from '@ant-design/icons';
 import { Dropdown, Input, MenuProps, Tree } from 'antd';
-import type { DataNode, TreeProps } from 'antd/es/tree';
+import type { DataNode } from 'antd/es/tree';
 import React, { ReactElement, useMemo, useState } from 'react';
 import cls from './index.module.less';
 
@@ -13,7 +13,6 @@ interface TreeType {
   childIcon?: ReactElement; //子级 --无下级 展示 icon
   handleTitleClick?: (node: any) => void; //名称 单击
   onDoubleClickTitle?: (node: any) => void; // 名称 双击
-  handleAddClick?: (node: any) => void; //展示 点击添加按钮事件
   handleMenuClick?: (_key: string, node: any) => void; //点击更多按钮事件
   title?: ReactElement | string;
   isDirectoryTree?: boolean; //是否文档树
@@ -47,7 +46,6 @@ const StoreClassifyTree: React.FC<TreeType> = ({
   childIcon,
   searchable = false,
   draggable = false,
-  handleAddClick,
   handleMenuClick,
   handleTitleClick,
   onDoubleClickTitle,
@@ -56,8 +54,9 @@ const StoreClassifyTree: React.FC<TreeType> = ({
 }) => {
   const [mouseOverItem, setMouseOverItem] = useState<any>({});
   const [searchValue, setSearchValue] = useState<string>('');
-  const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([]);
-  const [autoExpandParent, setAutoExpandParent] = useState(true);
+  // const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([]);
+  // const [autoExpandParent, setAutoExpandParent] = useState(true);
+  const [visibleMenu, setVisibleMenu] = useState(false);
   // 树形控件 更多操作
   const renderMenu: (data: any) => MenuProps['items'] = (data) => {
     if (menu === 'menus') {
@@ -80,15 +79,9 @@ const StoreClassifyTree: React.FC<TreeType> = ({
     });
   };
 
-  const onExpand = (newExpandedKeys: React.Key[]) => {
-    setExpandedKeys(newExpandedKeys);
-    setAutoExpandParent(false);
-  };
-
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     const newExpandedKeys = resetTreeData.map((item: any) => {
-      console.log('包含value', item.title.indexOf(value) > 0);
       if (item.title.indexOf(value) > 0) {
         return getParentKey(item.key, treeData);
       }
@@ -99,13 +92,11 @@ const StoreClassifyTree: React.FC<TreeType> = ({
     // );
     console.log('newExpandedKeys', newExpandedKeys);
 
-    setExpandedKeys(newExpandedKeys as React.Key[]);
+    // setExpandedKeys(newExpandedKeys as React.Key[]);
     setSearchValue(value);
-    setAutoExpandParent(true);
+    // setAutoExpandParent(true);
   };
   const resetTreeData = useMemo(() => {
-    console.log('sousuo', searchValue);
-
     const loop = (data: DataNode[]): DataNode[] =>
       data.map((item) => {
         const strTitle = item.title as string;
@@ -130,83 +121,13 @@ const StoreClassifyTree: React.FC<TreeType> = ({
           ...item,
         };
       });
-    console.log('打印测试', loop(treeData));
-
     return loop(treeData);
   }, [searchValue, treeData]);
-  const [gData, setGData] = useState([]);
 
-  const onDragEnter: TreeProps['onDragEnter'] = (info) => {
-    console.log('拖拽', info);
-    // expandedKeys 需要受控时设置
-    // setExpandedKeys(info.expandedKeys)
-  };
+  function onRightClick() {
+    setVisibleMenu(true);
+  }
 
-  const onDrop: TreeProps['onDrop'] = (info) => {
-    console.log('拖拽2', info);
-    const dropKey = info.node.key;
-    const dragKey = info.dragNode.key;
-    const dropPos = info.node.pos.split('-');
-    const dropPosition = info.dropPosition - Number(dropPos[dropPos.length - 1]);
-
-    const loop = (
-      data: DataNode[],
-      key: React.Key,
-      // eslint-disable-next-line no-unused-vars
-      callback: (node: DataNode, i: number, data: DataNode[]) => void,
-    ) => {
-      for (let i = 0; i < data.length; i++) {
-        if (data[i].key === key) {
-          return callback(data[i], i, data);
-        }
-        if (data[i].children) {
-          loop(data[i].children!, key, callback);
-        }
-      }
-    };
-    const data = [...gData];
-
-    // Find dragObject
-    let dragObj: DataNode;
-    loop(data, dragKey, (item, index, arr) => {
-      arr.splice(index, 1);
-      dragObj = item;
-    });
-
-    if (!info.dropToGap) {
-      // Drop on the content
-      loop(data, dropKey, (item) => {
-        item.children = item.children || [];
-        // where to insert 示例添加到头部，可以是随意位置
-        item.children.unshift(dragObj);
-      });
-    } else if (
-      ((info.node as any).props.children || []).length > 0 && // Has children
-      (info.node as any).props.expanded && // Is expanded
-      dropPosition === 1 // On the bottom gap
-    ) {
-      loop(data, dropKey, (item) => {
-        item.children = item.children || [];
-        // where to insert 示例添加到头部，可以是随意位置
-        item.children.unshift(dragObj);
-        // in previous version, we use item.children.push(dragObj) to insert the
-        // item to the tail of the children
-      });
-    } else {
-      let ar: DataNode[] = [];
-      let i: number;
-      loop(data, dropKey, (_item, index, arr) => {
-        ar = arr;
-        i = index;
-      });
-      if (dropPosition === -1) {
-        ar.splice(i!, 0, dragObj!);
-      } else {
-        ar.splice(i! + 1, 0, dragObj!);
-      }
-    }
-    setGData(data);
-  };
   const renderTreeTitle = (node: any) => {
     return (
       <div
@@ -216,6 +137,7 @@ const StoreClassifyTree: React.FC<TreeType> = ({
         }}
         onMouseLeave={() => {
           setMouseOverItem({});
+          setVisibleMenu(false);
         }}>
         <div
           className={cls.treeTitleBoxLabel}
@@ -229,14 +151,6 @@ const StoreClassifyTree: React.FC<TreeType> = ({
         <div className={cls.treeTitleBoxBtns} onClick={(e: any) => e.stopPropagation()}>
           {mouseOverItem.key === node.key ? (
             <>
-              {handleAddClick ? (
-                <PlusOutlined
-                  className={cls.titleIcon}
-                  onClick={() => handleAddClick(node)}
-                />
-              ) : (
-                ''
-              )}
               {menu ? (
                 <Dropdown
                   menu={{
@@ -246,7 +160,11 @@ const StoreClassifyTree: React.FC<TreeType> = ({
                     },
                   }}
                   placement="bottom"
-                  trigger={['click']}>
+                  open={visibleMenu}
+                  onOpenChange={(open: boolean) => {
+                    setVisibleMenu(open);
+                  }}
+                  trigger={['click', 'contextMenu']}>
                   <EllipsisOutlined className={cls.titleIcon} rotate={90} />
                 </Dropdown>
               ) : (
@@ -260,6 +178,7 @@ const StoreClassifyTree: React.FC<TreeType> = ({
       </div>
     );
   };
+
   return (
     <div className={cls.customTreeWrap}>
       {title && typeof title === 'string' ? (
@@ -275,26 +194,21 @@ const StoreClassifyTree: React.FC<TreeType> = ({
       {isDirectoryTree ? (
         <DirectoryTree
           className={className}
-          // switcherIcon={<LeftCircleOutlined />}
-          titleRender={renderTreeTitle}
-          defaultExpandedKeys={expandedKeys}
-          // draggable={draggable}
-          onDragEnter={onDragEnter}
-          onDrop={onDrop}
+          titleRender={menu ? renderTreeTitle : undefined}
+          onRightClick={() => {
+            setVisibleMenu(true);
+          }}
           treeData={treeData}
           {...rest}
         />
       ) : (
         <Tree
-          className="draggable-tree"
-          // switcherIcon={<LeftCircleOutlined />}
-          titleRender={renderTreeTitle}
-          onDragEnter={onDragEnter}
-          onDrop={onDrop}
+          className={className}
+          titleRender={menu ? renderTreeTitle : undefined}
+          onRightClick={() => {
+            setVisibleMenu(true);
+          }}
           treeData={resetTreeData}
-          onExpand={onExpand}
-          expandedKeys={expandedKeys}
-          autoExpandParent={autoExpandParent}
           {...rest}
         />
       )}
