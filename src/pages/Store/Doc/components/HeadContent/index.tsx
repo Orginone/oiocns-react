0430cdd7f0618ebc9997f0aa28b85effd11968e7
cclu <lkrnchocolateaaron@gmail.com>
2022-11-30 19:15:39 +0800

    fix: 文档组件抽离代码整理

---
+++ src/pages/Store/Doc/components/HeadContent/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Breadcrumb, Divider, Space, Typography, Upload, UploadProps } from 'antd';
+import { docsCtrl } from '@/ts/controller/store/docsCtrl';
+import { IFileSystemItem } from '@/ts/core/store/ifilesys';
+import { ImSpinner9, ImArrowUp2, ImUpload } from 'react-icons/im';
+import { CaretRightOutlined, FolderAddFilled } from '@ant-design/icons';
+
+const getBreadcrumb = (key: string, items: any[]) => {
+  const item = docsCtrl.refItem(key);
+  if (item) {
+    items.unshift({
+      key: item.key,
+      label: item.name,
+    });
+    if (item.parent) {
+      items = getBreadcrumb(item.parent.key, items);
+    }
+  }
+  return items;
+};
+
+const CardTiltle = ({
+  handleMenuClick,
+  current,
+  uploadRef,
+  props,
+}: {
+  handleMenuClick: (key: string, node: IFileSystemItem) => void;
+  props: UploadProps;
+  current?: IFileSystemItem;
+  uploadRef: React.MutableRefObject<any>;
+}) => {
+  return (
+    <Space wrap split={<Divider type="vertical" />} size={2}>
+      <Typography.Link
+        disabled={current?.parent == undefined ?? false}
+        onClick={() => docsCtrl.backup()}>
+        <ImArrowUp2 />
+      </Typography.Link>
+      <Typography.Link onClick={() => handleMenuClick('刷新', {} as IFileSystemItem)}>
+        <ImSpinner9 />
+      </Typography.Link>
+      <Upload {...props} ref={uploadRef}>
+        <Typography.Link style={{ fontSize: 18 }}>
+          <ImUpload />
+        </Typography.Link>
+      </Upload>
+      <Typography.Link
+        onClick={() => handleMenuClick('新建文件夹', {} as IFileSystemItem)}>
+        <FolderAddFilled />
+      </Typography.Link>
+      <Breadcrumb separator={<CaretRightOutlined />}>
+        {getBreadcrumb(current?.key ?? '', []).map((item) => {
+          return (
+            <Breadcrumb.Item key={item.key}>
+              <a onClick={async () => await docsCtrl.open(item.key)}>{item.label}</a>
+            </Breadcrumb.Item>
+          );
+        })}
+      </Breadcrumb>
+    </Space>
+  );
+};
+export default CardTiltle;
--- src/pages/Store/Doc/index.tsx
+++ src/pages/Store/Doc/index.tsx
@@ -1,11 +1,240 @@
-import React from 'react';
-import Content from './content';
+import React, { useEffect, useRef, useState } from 'react';
+import { Segmented, Card, Typography, UploadProps } from 'antd';
+import { docsCtrl } from '@/ts/controller/store/docsCtrl';
+import { RcFile } from 'antd/lib/upload/interface';
+import { IFileSystemItem, IObjectItem } from '@/ts/core/store/ifilesys';
+import Plan, { TaskModel } from './plan';
+import ResetNameModal from './components/ResetName';
+import { FaHourglassEnd, FaHourglassHalf } from 'react-icons/fa';
+import CoppyOrMove from './components/CoppyOrMove';
+import TableContent from './components/TableContent';
+import CardTiltle from './components/HeadContent';
+import CardListContent from './components/CardContent';
+import { IconFont } from '@/components/IconFont';
+import cls from './index.module.less';
+
+type NameValue = {
+  name: string;
+  value: string;
+};
 /**
  * 仓库-文档
  * @returns
  */
 const StoreDoc: React.FC = () => {
-  return <Content></Content>;
-};
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState('新建文件夹');
+  const [coppyOrMoveTitle, setCoppyOrMoveTitle] = useState('复制到');
+  const [currentTarget, setCurrentTarget] = useState<IObjectItem>();
+  const [reNameKey, setReNameKey] = useState<string | undefined>('');
+  const [segmentedValue, setSegmentedValue] = useState<'Kanban' | 'List'>('Kanban');
+  const [taskList, setTaskList] = useState<TaskModel[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [moveModalOpen, setMoveModalOpen] = useState(false);
+  const [current, setCurrent] = useState(docsCtrl.current);
+  const [pageData, setPagedata] = useState<IFileSystemItem[]>([]);
+  const [createFileName, setCreateFileName] = useState<string>('');
+  const [dicExtension, setDicExtension] = useState<NameValue[]>([]);
+  const uploadRef = useRef<any>();
+  const parentRef = useRef<any>();
+  const refreshUI = () => {
+    if (docsCtrl.current != undefined) {
+      setPagedata([...docsCtrl.current.children]);
+      setCurrent({ ...docsCtrl.current });
+    }
+  };
+  useEffect(() => {
+    fetch('/fileext.json').then(async (res) => {
+      const temp: NameValue[] = await res.json();
+      if (temp && temp.length > 0) {
+        setDicExtension(temp);
+      }
+    });
+    const id = docsCtrl.subscribe(refreshUI);
+    return () => {
+      docsCtrl.unsubscribe(id);
+    };
+  }, []);
+  const Uploading = () => {
+    for (const t of taskList) {
+      if (t.process < 1) {
+        return true;
+      }
+    }
+    return false;
+  };
+  const uploadProps: UploadProps = {
+    multiple: true,
+    showUploadList: false,
+    async customRequest(options) {
+      if (docsCtrl.current) {
+        const file: RcFile = options.file as RcFile;
+        const task: TaskModel = {
+          process: 0,
+          name: file.name,
+          size: file.size,
+          createTime: new Date(),
+          group: docsCtrl.current.name,
+        };
+        docsCtrl.current?.upload(file.name, file, (p) => {
+          if (p === 0) {
+            taskList.push(task);
+          }
+          task.process = p;
+          setTaskList([...taskList]);
+        });
+      }
+    },
+  };
+  const getThumbnail = (item: IFileSystemItem) => {
+    if (item.target.thumbnail.length > 0) {
+      return item.target.thumbnail;
+    }
+    let prifex = '/icons/';
+    if (item.target.extension === '') {
+      return prifex + 'default_folder.svg';
+    } else {
+      for (const d of dicExtension) {
+        if (d.name === item.target.extension.toLowerCase()) {
+          return prifex + 'file_type_' + d.value + '.svg';
+        }
+      }
+    }
+    return prifex + 'default_file.svg';
+  };
+  const getPreview = (el: IFileSystemItem) => {
+    if (el.target.thumbnail?.length > 0) {
+      return {
+        src: '/orginone/anydata/bucket/load/' + el.target.shareLink,
+      };
+    }
+    return false;
+  };
+  const handleMenuClick = async (key: string, node: any) => {
+    switch (key) {
+      case '1': // 删除
+        if (await docsCtrl.refItem(node.key)?.delete()) {
+          docsCtrl.changCallback();
+        }
+        break;
+      case '2': // 重命名
+        setReNameKey(node.key);
+        setCreateFileName(node.name);
+        setTitle('重命名');
+        setIsModalOpen(true);
+        break;
+      case '3': // 移动到
+        setCurrentTarget(docsCtrl.refItem(node.key));
+        setCoppyOrMoveTitle('移动到');
+        setMoveModalOpen(true);
+        break;
+      case '4': // 复制到
+        setCurrentTarget(docsCtrl.refItem(node.key));
+        setCoppyOrMoveTitle('复制到');
+        setMoveModalOpen(true);
+        break;
+      case '5': // 下载
+        break;
+      case '刷新': // 刷新
+        docsCtrl.current?.loadChildren(true);
+        docsCtrl.changCallback();
+        break;
+      case '新建文件夹': // 新建文件夹
+        setReNameKey(undefined);
+        setTitle('新建文件夹');
+        setCreateFileName('');
+        setIsModalOpen(true);
+        break;
+      case '上传': // 刷新
+        if (uploadRef && uploadRef.current && uploadRef.current.upload) {
+          uploadRef.current.upload.uploader.onClick();
+        }
+        break;
+      default:
+        break;
+    }
+  };
+  return (
+    <Card
+      className={cls.pageCard}
+      title={
+        <CardTiltle
+          handleMenuClick={handleMenuClick}
+          props={uploadProps}
+          current={current}
+          uploadRef={uploadRef}
+        />
+      }
+      extra={
+        <Typography.Link onClick={() => setOpen(true)}>
+          {Uploading() ? <FaHourglassHalf /> : <FaHourglassEnd />}
+        </Typography.Link>
+      }
+      bordered={false}>
+      <div className={cls.mainContent} ref={parentRef}>
+        {segmentedValue === 'List' ? (
+          <TableContent
+            parentRef={parentRef}
+            pageData={pageData}
+            getThumbnail={getThumbnail}
+            getPreview={getPreview}
+            handleMenuClick={handleMenuClick}
+          />
+        ) : (
+          <CardListContent
+            pageData={pageData}
+            getThumbnail={getThumbnail}
+            handleMenuClick={handleMenuClick}
+            getPreview={getPreview}
+          />
+        )}
+      </div>
+      <Segmented
+        value={segmentedValue}
+        onChange={(value) => setSegmentedValue(value as 'Kanban' | 'List')}
+        options={[
+          {
+            value: 'List',
+            icon: (
+              <IconFont
+                type={'icon-chuangdanwei'}
+                className={segmentedValue === 'List' ? 'active' : ''}
+              />
+            ),
+          },
+          {
+            value: 'Kanban',
+            icon: (
+              <IconFont
+                type={'icon-jianyingyong'}
+                className={segmentedValue === 'Kanban' ? 'active' : ''}
+              />
+            ),
+          },
+        ]}
+      />
+      <ResetNameModal
+        title={title}
+        open={isModalOpen}
+        reNameKey={reNameKey}
+        value={createFileName}
+        onChange={setIsModalOpen}
+      />
+      <CoppyOrMove
+        currentTaget={currentTarget}
+        open={moveModalOpen}
+        title={coppyOrMoveTitle}
+        onChange={setMoveModalOpen}
+      />
 
+      <Plan
+        isOpen={open}
+        taskList={taskList}
+        onClose={() => {
+          setOpen(false);
+        }}
+      />
+    </Card>
+  );
+};
 export default StoreDoc;
