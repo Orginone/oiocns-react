f811e8d51258ea34a916d4bc0968c9a988bc3c79
zhangqiang <1196217890@qq.com>
2022-12-02 14:01:09 +0800

    add:调整代码

--- src/ts/core/setting/setting.ts
+++ src/ts/core/setting/setting.ts
@@ -1,20 +1,14 @@
 import { ISetting } from './isetting';
 /**
  * 设置的基类
- * */ 
+ * */
 
-class baseSetting implements ISetting { 
-
-}
+class baseSetting implements ISetting {}
 /**
  * 部门设置的setting
- * */ 
-class deptStting extends baseSetting { 
-  
-}
+ * */
+class deptStting extends baseSetting {}
 
-export const loadDifSetting = () => { 
+export const loadDifSetting = () => {
   return new deptStting();
-}
-
-
+};
