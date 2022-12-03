93d8b603b9ce1f6042a878a79918f42baff626da
linlh <a2337175092@163.com>
2022-12-03 22:02:16 +0800

    fix: 数据

--- src/ts/controller/setting/userCtrl.ts
+++ src/ts/controller/setting/userCtrl.ts
@@ -34,18 +34,18 @@ class UserController extends BaseController {
   }
   /** 当前用户 */
   get User(): IPerson {
-    if(this._user){
+    if (this._user) {
       return this._user;
-    }else{
-      return { id: '', target: {id: ''} } as unknown as Person;
+    } else {
+      return { id: '', target: { id: '' } } as unknown as Person;
     }
   }
   /** 当前单位空间 */
   get Space(): ICompany {
-    if(this._curSpace){
+    if (this._curSpace) {
       return this._curSpace;
-    }else{
-      return { id: '', target: {id: ''} } as unknown as ICompany;
+    } else {
+      return { id: '', target: { id: '' } } as unknown as ICompany;
     }
   }
   /** 当前空间数据 */
