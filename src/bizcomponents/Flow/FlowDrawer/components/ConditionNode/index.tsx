f0e1648fff7befc3fd5a135b9d097bf73ec79dde
zhangqiang <1196217890@qq.com>
2022-12-06 10:25:22 +0800

    test:修改字段和值反了的bug

--- src/bizcomponents/Flow/FlowDrawer/components/ConditionNode/index.tsx
+++ src/bizcomponents/Flow/FlowDrawer/components/ConditionNode/index.tsx
@@ -14,7 +14,7 @@ const ConditionNode = () => {
   const [key, setKey] = useState(0);
   /**点击添加的时候默认增加一行 */
   const addConditionGroup = () => {
-    console.timeLog('selectedNode.conditions', selectedNode.conditions);
+    console.log('selectedNode.conditions', selectedNode.conditions);
     selectedNode.conditions?.push({
       pos: selectedNode.conditions.length + 1,
       paramKey: '',
@@ -24,14 +24,13 @@ const ConditionNode = () => {
       type: 'NUMERIC',
       val: null,
     });
-
     setSelectedNode(selectedNode);
     setKey(key + 1);
   };
 
   return (
     <div>
-      <div>
+      <div style={{ marginBottom: '10px' }}>
         <Button type="primary" onClick={addConditionGroup}>
           添加条件
         </Button>
