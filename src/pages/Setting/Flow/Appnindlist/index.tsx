8b87d0ada4a9815a4da12b7a50b4b2a9e2701c4e
zhangqiang <1196217890@qq.com>
2022-12-10 18:21:34 +0800

    add:修改

--- src/pages/Setting/Flow/Appnindlist/index.tsx
+++ src/pages/Setting/Flow/Appnindlist/index.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Card, Avatar, Space } from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import SelfAppCtrl from '@/ts/controller/store/selfAppCtrl';
+import userCtrl from '@/ts/controller/setting/userCtrl';
+
 import cls from './index.module.less';
 
 const { Meta } = Card;
@@ -15,12 +17,18 @@ const AppBindList: React.FC<AppBindListprops> = ({ bindAppMes }) => {
 
   useEffect(() => {
     initData();
-  }, []);
+  }, [bindAppMes]);
 
   const initData = async () => {
-    const getAppDataList = await SelfAppCtrl.querySelfApps();
-    console.log(getAppDataList);
-    setAppDataList(getAppDataList);
+    console.log('bindAppMes', bindAppMes);
+    const result = await userCtrl.space.getDefines(false);
+    const currentValue = await userCtrl.space.queryFlowRelation(false);
+    if (currentValue && currentValue.length > 0) {
+      const filterId = currentValue.filter((item) => {
+        return item.defineId === result[0].id;
+      });
+      setAppDataList(filterId);
+    }
   };
 
   return (
@@ -32,25 +40,27 @@ const AppBindList: React.FC<AppBindListprops> = ({ bindAppMes }) => {
       }
       bordered={false}>
       <Space className={cls.appwrap}>
-        {appDataList.map((item: any) => {
-          return (
-            <div key={item.prod.id}>
-              <Card
-                style={{ width: 300 }}
-                actions={[
-                  <SettingOutlined key="setting" />,
-                  <EditOutlined key="edit" />,
-                  <EllipsisOutlined key="ellipsis" />,
-                ]}>
-                <Meta
-                  avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                  title={item.prod.name}
-                  description={item.prod.remark || '暂无描述'}
-                />
-              </Card>
-            </div>
-          );
-        })}
+        {appDataList &&
+          appDataList.map((item: any) => {
+            return (
+              <div key={item.prod?.id}>
+                <Card
+                  style={{ width: 300 }}
+                  actions={[
+                    <SettingOutlined key="setting" />,
+                    <EditOutlined key="edit" />,
+                    <EllipsisOutlined key="ellipsis" />,
+                  ]}>
+                  <Meta
+                    avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                    title={item.prod?.name}
+                    description={item.prod?.remark || '暂无描述'}
+                  />
+                </Card>
+              </div>
+            );
+          })}
+        {!(appDataList.length > 0) ? '暂无数据' : null}
       </Space>
     </Card>
   );
