f88970f9097737320114c6def72bafd0a5d1e43c
939097257@qq.com <939097257@qq.com>
2022-12-13 13:38:35 +0800

    fix:样式问题，多余代码

--- src/pages/Person/Friend/index.tsx
+++ src/pages/Person/Friend/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import CardOrTable from '@/components/CardOrTableComp';
-import LookApply from '../../Setting/Dept/components/LookApply';
 import cls from './index.module.less';
 import Title from 'antd/lib/typography/Title';
 import { Modal, Button, message } from 'antd';
