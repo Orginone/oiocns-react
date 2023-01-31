3760833f7c9ee854057ccc49a711d3247b4c7641
jurendada <944285634@qq.com>
2023-01-31 23:10:16 +0800

    feat: 设计表单时选择制定组织

--- src/pages/Setting/components/formDesignModal.tsx
+++ src/pages/Setting/components/formDesignModal.tsx
@@ -3,9 +3,9 @@ import { OperationModel } from '@/ts/base/model';
 import { XOperation, XOperationItem } from '@/ts/base/schema';
 import { ITarget } from '@/ts/core';
 import { ISpeciesItem } from '@/ts/core/target/species/ispecies';
-import { Modal } from 'antd';
+import { Modal, TreeSelect } from 'antd';
 import { FRGeneratorProps } from 'fr-generator';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import userCtrl from '@/ts/controller/setting';
 import { isNumber } from '@/ts/base/common';
 import FormDesign from './formDesign';
@@ -30,6 +30,17 @@ interface FormDesignModalProps extends FRGeneratorProps {
 */
 const FormDesignModal = (props: FormDesignModalProps) => {
   let { title, open, data, handleCancel, handleOk, current } = props;
+  const [belongId, setBelongId] = useState<string>(userCtrl.space.id);
+
+  const [treeData, setTreeData] = useState<any[]>();
+
+  useEffect(() => {
+    const loadTeam = async () => {
+      const res = await userCtrl.getTeamTree();
+      setTreeData(res);
+    };
+    loadTeam();
+  }, []);
 
   data = data as XOperation;
   let schema: any = undefined;
@@ -44,7 +55,7 @@ const FormDesignModal = (props: FormDesignModalProps) => {
   const queryItems = async () => {
     const res = await kernel.queryOperationItems({
       id: data.id,
-      spaceId: userCtrl.space.id,
+      spaceId: belongId,
       page: { offset: 0, limit: 100000, filter: '' },
     });
     operationItems = res.data.result || [];
@@ -57,10 +68,13 @@ const FormDesignModal = (props: FormDesignModalProps) => {
   // 刷新Items
   const refresh = (newSchema: any) => {
     schema = newSchema;
+    console.log('newSchema', newSchema);
+    const keys: string[] = [];
     for (const key in schema.properties) {
       if (Object.prototype.hasOwnProperty.call(schema.properties, key) && key) {
         const element = schema.properties[key];
         if (isNumber(Number(key))) {
+          keys.push(key);
           // 在 operationItems 是否存在
           operationItems.forEach((item) => {
             if (key === item.id && item.id) {
@@ -70,7 +84,7 @@ const FormDesignModal = (props: FormDesignModalProps) => {
                 code: element.bind || key,
                 rule: JSON.stringify(element),
                 remark: element.type,
-                belongId: userCtrl.space.id,
+                belongId,
                 operationId: data.id,
               };
               let index = updateItems.findIndex((i) => i.id === updateItem.id);
@@ -88,7 +102,7 @@ const FormDesignModal = (props: FormDesignModalProps) => {
             code: element.bind || key,
             rule: JSON.stringify(element),
             remark: element.type,
-            belongId: userCtrl.space.id,
+            belongId,
             operationId: data.id,
           };
           let index = createItems.findIndex((i) => i.id === createItem.id);
@@ -100,27 +114,31 @@ const FormDesignModal = (props: FormDesignModalProps) => {
         }
       }
     }
-    const updateItemIds = updateItems.map((item) => item.id);
     const ids = operationItems
-      .filter((i) => !updateItemIds.includes(i.id) && i.id)
+      .filter((i) => !keys.includes(i.id) && i.id)
       .map((i) => i.id);
     deleteItemIds.clear();
     for (const id of ids) {
       deleteItemIds.add(id);
     }
-    createItems = createItems.filter((i) => !updateItemIds.includes(i.id));
+    createItems = createItems.filter((i) => !keys.includes(i.id as string));
+
+    console.log('createItems', createItems);
+    console.log('updateItems', updateItems);
+    console.log('deleteItemIds', deleteItemIds);
   };
+
   // 保存
   const save = async () => {
-    const allItems: XOperationItem[] = [];
     for (const item of createItems) {
       item.id = undefined;
       const res = await kernel.createOperationItem(item);
-      allItems.push(res.data);
+      schema.properties[res.data.id] = JSON.parse(item.rule);
     }
     for (const item of updateItems) {
+      schema.properties[item.id as string] = JSON.parse(item.rule);
       const res = await kernel.updateOperationItem(item);
-      allItems.push(res.data);
+      console.log(res);
     }
     // TODO 删除时判断归属权限
     deleteItemIds.forEach(async (id) => {
@@ -129,14 +147,28 @@ const FormDesignModal = (props: FormDesignModalProps) => {
         typeName: '',
       });
       console.log(res);
+      delete schema.properties[id];
     });
-    operationItems = allItems;
-    if (allItems.length > 0) {
-      schema.properties = {};
-      for (const item of allItems) {
-        schema.properties[item.id] = JSON.parse(item.rule);
+    const ids: string[] = [];
+    for (const key in schema.properties) {
+      if (Object.prototype.hasOwnProperty.call(schema.properties, key) && key) {
+        if (isNumber(Number(key))) {
+          ids.push(key);
+          const element = schema.properties[key];
+          for (const item of operationItems) {
+            if (item.id === key) {
+              item.name = element.title;
+              item.code = element.bind || key;
+              item.rule = JSON.stringify(element);
+              item.remark = element.type;
+            }
+          }
+        } else {
+          delete schema.properties[key];
+        }
       }
     }
+    operationItems = operationItems.filter((i) => ids.includes(i.id));
 
     const body: OperationModel = {
       id: data?.id,
@@ -160,17 +192,28 @@ const FormDesignModal = (props: FormDesignModalProps) => {
       onCancel={handleCancel}
       maskClosable={false}
       width={1280}>
-      <div style={{ paddingTop: '14px' }}>
-        <FormDesign
-          defaultValue={schema}
-          onSchemaChange={(newSchema) => {
-            if (newSchema) {
-              refresh(newSchema);
-            }
-          }}
-          current={current}
+      <div style={{ padding: '8px' }}>
+        <label>制定组织：</label>
+        <TreeSelect
+          style={{ width: '30%' }}
+          defaultValue={userCtrl.space.id}
+          value={belongId}
+          dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+          treeData={treeData}
+          placeholder="请选择制定组织"
+          fieldNames={{ label: 'teamName', value: 'id', children: 'subTeam' }}
+          onChange={(value) => setBelongId(value)}
         />
       </div>
+      <FormDesign
+        defaultValue={schema}
+        onSchemaChange={(newSchema) => {
+          if (newSchema) {
+            refresh(newSchema);
+          }
+        }}
+        current={current}
+      />
     </Modal>
   );
 };
