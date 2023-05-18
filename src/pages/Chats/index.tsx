a83b9fefc4e81e173ff2b950ccb437edead3d54f
SEN <changsen25@163.com>
2023-05-18 09:21:38 +0800

    fix: 删除监察展示

--- src/pages/Chats/index.tsx
+++ src/pages/Chats/index.tsx
@@ -1,22 +1,15 @@
 import React, { useState } from 'react';
 import Content from './content';
-import Supervise from './components/Supervise';
 import * as config from './config/menuOperate';
 import MainLayout from '@/components/MainLayout';
 import useMenuUpdate from '@/hooks/useMenuUpdate';
-import { Input, Button } from 'antd';
+import { Input } from 'antd';
 import { ImSearch } from 'react-icons/im';
-import { orgAuth } from '@/ts/core/public/consts';
 import { IMsgChat, msgChatNotify } from '@/ts/core';
 const Setting: React.FC<any> = () => {
   const [filter, setFilter] = useState('');
   const [openDetail, setOpenDetail] = useState<boolean>(false);
-  const [isSupervise, setIsSupervise] = useState<boolean>(false);
   const [key, rootMenu, selectMenu, setSelectMenu] = useMenuUpdate(config.loadChatMenu);
-
-  const changeSupervise = () => {
-    setIsSupervise(false);
-  };
   if (!selectMenu || !rootMenu) return <></>;
   return (
     <MainLayout
@@ -26,31 +19,6 @@ const Setting: React.FC<any> = () => {
       }}
       rightBar={
         <React.Fragment>
-          {selectMenu?.company?.hasAuthoritys([orgAuth.RelationAuthId]) ? (
-            <React.Fragment>
-              {isSupervise ? (
-                <Button
-                  type="primary"
-                  size="small"
-                  onClick={() => {
-                    setIsSupervise(false);
-                  }}>
-                  沟通
-                </Button>
-              ) : (
-                <Button
-                  type="primary"
-                  size="small"
-                  onClick={() => {
-                    setIsSupervise(true);
-                  }}>
-                  监察
-                </Button>
-              )}
-            </React.Fragment>
-          ) : (
-            ''
-          )}
           <Input
             style={{ height: 30, fontSize: 15 }}
             placeholder="搜索"
@@ -78,21 +46,12 @@ const Setting: React.FC<any> = () => {
         }
       }}
       siderMenuData={rootMenu}>
-      {isSupervise ? (
-        <Supervise
-          key={key}
-          selectMenu={selectMenu}
-          current={selectMenu.company}
-          changeSupervise={changeSupervise}
-        />
-      ) : (
-        <Content
-          key={key}
-          selectMenu={selectMenu}
-          openDetail={openDetail}
-          filter={filter}
-        />
-      )}
+      <Content
+        key={key}
+        selectMenu={selectMenu}
+        openDetail={openDetail}
+        filter={filter}
+      />
     </MainLayout>
   );
 };
