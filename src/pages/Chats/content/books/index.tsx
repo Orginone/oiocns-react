2f8dfb0f38cbed4bf76921a8d1556c79177fa5d5
wangbo <wb03885132@163.com>
2023-05-27 16:36:05 +0800

    fix: 处理通讯录展示问题

--- src/pages/Chats/content/books/index.tsx
+++ src/pages/Chats/content/books/index.tsx
@@ -214,8 +214,8 @@ const Book: React.FC<any> = ({
                           })}
                       </div>
                     }
-                    description={showMessage(item)}
-                    // description={item.information}
+                    // description={showMessage(item)}
+                    description={item.information}
                   />
                 </List.Item>
               </div>
