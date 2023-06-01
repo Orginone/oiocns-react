1f9e7a036d78f73de2f3fc8cc9699a2780c11190
激扬 <1731964774@qq.com>
2023-06-01 11:33:50 +0800

    🐞 fix: 已读未读优化

--- src/pages/Chats/content/chat/GroupContent/information.tsx
+++ src/pages/Chats/content/chat/GroupContent/information.tsx
@@ -1,12 +1,16 @@
 import EntityIcon from '@/bizcomponents/GlobalComps/entityIcon';
 import { IMessage } from '@/ts/core';
 import css from './index.module.less';
-import { Drawer } from 'antd';
-import React from 'react';
+import { Drawer, Tabs } from 'antd';
+import React, { useState } from 'react';
+import { showChatTime } from '@/utils/tools';
+import type { TabsProps } from 'antd';
 
 const Information = ({ msg, onClose }: { msg: IMessage; onClose: Function }) => {
-  return (
-    <Drawer title={'消息标记信息'} onClose={() => onClose()} closable open>
+  const [tabsKey, setTabsKey] = useState<string>();
+  // 展示已读的
+  const readList = () => {
+    return (
       <ul className={css.moreInfo}>
         {msg.labels.map((i) => {
           return (
@@ -14,13 +18,56 @@ const Information = ({ msg, onClose }: { msg: IMessage; onClose: Function }) => 
               <EntityIcon share={i.labeler} fontSize={22} size={30} />
               <strong>{i.labeler.name}</strong>
               <div>
-                <span>{i.time}:</span>
+                <span>{showChatTime(i.time)}:</span>
                 <strong>{i.label}</strong>
               </div>
             </li>
           );
         })}
       </ul>
+    );
+  };
+  // 展示未读
+  const unRead = () => {
+    const unreadList = msg._chat.members?.filter((v) => {
+      if (msg.labels.length > 0) {
+        return msg?.labels?.find((prop) => prop.userId !== v.id);
+      } else {
+        return v;
+      }
+    });
+    return (
+      <ul className={css.moreInfo}>
+        {unreadList
+          ?.filter((code) => code.id !== msg._chat.userId)
+          .map((i) => {
+            return (
+              <li key={i.time}>
+                <EntityIcon share={i.icon} fontSize={22} size={30} />
+                <strong>{i.name}</strong>
+                <div>
+                  <strong>{i.label}</strong>
+                </div>
+              </li>
+            );
+          })}
+      </ul>
+    );
+  };
+
+  const items: TabsProps['items'] = [
+    { key: 'read', label: '已读', children: readList() },
+    { key: 'Unread', label: '未读', children: unRead() },
+  ];
+
+  return (
+    <Drawer title={'消息标记信息'} onClose={() => onClose()} closable open>
+      <Tabs
+        items={items}
+        defaultActiveKey={'read'}
+        activeKey={tabsKey}
+        onChange={(e) => setTabsKey(e)}
+      />
     </Drawer>
   );
 };
