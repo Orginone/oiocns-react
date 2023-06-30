c255d4aeab913d915357a43385a0509c7c9b64a9
realVeer <realVeer@qkbyte.com>
2023-06-30 22:59:34 +0800

    fix: 手误this指针

--- src/ts/base/api/kernelapi.ts
+++ src/ts/base/api/kernelapi.ts
@@ -26,7 +26,7 @@ export default class KernelApi {
     this._methods = {};
     this._anystore = AnyStore.getInstance();
     this._storeHub = new StoreHub(url, 'json');
-    this._storeHub.on('Receive', this._receive);
+    this._storeHub.on('Receive', (res) => this._receive(res));
     this._storeHub.onConnected(() => {
       if (this._anystore.accessToken.length > 0) {
         this._storeHub
