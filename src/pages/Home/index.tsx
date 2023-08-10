22e74dad41d61ca9e3e244ca04811b401090730e
IlhamTahir <yantaq@bilig.biz>
2023-08-10 09:06:38 +0800

    fix(homepage): 首页数字资产、好友圈完成

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,146 +1,54 @@
 import cls from './index.module.less';
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import HeadBanner from '@/pages/Home/components/HeadBanner';
-import { Avatar, Card, Col, List, Row } from 'antd';
 import NavigationBar from '@/pages/Home/components/NavigationBar';
 
+export interface NavigationItem {
+  key: string;
+  label: string;
+  backgroundImageUrl: string;
+  component: any;
+}
+const navigationList: NavigationItem[] = [
+  {
+    key: 'activity',
+    label: '群动态',
+    backgroundImageUrl: '/img/banner/activity-bg.png',
+    component: React.lazy(() => import('@/pages/Home/components/Content/Activity')),
+  },
+  {
+    key: 'circle',
+    label: '好友圈',
+    backgroundImageUrl: '/img/banner/circle-bg.jpeg',
+    component: React.lazy(() => import('@/pages/Home/components/Content/Circle')),
+  },
+  {
+    key: 'warehouse',
+    label: '公物仓',
+    backgroundImageUrl: '/img/banner/activity-bg.png',
+    component: React.lazy(() => import('@/pages/Home/components/Content/Warehouse')),
+  },
+  {
+    key: 'digital-asset',
+    label: '数据资产',
+    backgroundImageUrl: '/img/banner/digital-asset-bg.png',
+    component: React.lazy(() => import('@/pages/Home/components/Content/DigitalAsset')),
+  },
+];
 const Home: React.FC = () => {
-  useEffect(() => {}, []);
-
-  const data = Array.from({ length: 23 }).map((_, i) => ({
-    href: '#',
-    title: `名字名字${i}`,
-    avatar: 'https://joeschmoe.io/api/v1/random',
-    content:
-      '动态内容动态内容动态内容动态内容动态内容动态内容动态内容动态内容动态内容动态内容动态内容动态内容动态内容动态内容...',
-  }));
+  const [current, setCurrent] = useState(navigationList[0]);
 
   return (
     <div className={cls.homepage}>
-      <NavigationBar></NavigationBar>
+      <NavigationBar
+        list={navigationList}
+        onChange={(item) => {
+          setCurrent(item);
+        }}></NavigationBar>
       <HeadBanner
-        backgroundImageUrl={'/img/banner/activity-bg.png'}
-        title="群动态"></HeadBanner>
-      <Row gutter={[24, 24]} className={cls.content}>
-        <Col span={12}>
-          <Card title="经常浏览">
-            <List
-              itemLayout="vertical"
-              size="large"
-              dataSource={data}
-              footer={
-                <div>
-                  <b>ant design</b> footer part
-                </div>
-              }
-              renderItem={(item) => (
-                <List.Item
-                  key={item.title}
-                  extra={
-                    <img
-                      width={272}
-                      alt="logo"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                    />
-                  }>
-                  <List.Item.Meta
-                    avatar={<Avatar src={item.avatar} />}
-                    title={<a href={item.href}>{item.title}</a>}
-                  />
-                  {item.content}
-                </List.Item>
-              )}
-            />
-          </Card>
-        </Col>
-        <Col span={12}>
-          <List
-            itemLayout="vertical"
-            size="large"
-            dataSource={data}
-            footer={
-              <div>
-                <b>ant design</b> footer part
-              </div>
-            }
-            renderItem={(item) => (
-              <List.Item key={item.title}>
-                <List.Item.Meta
-                  avatar={<Avatar src={item.avatar} />}
-                  title={<a href={item.href}>{item.title}</a>}
-                />
-                {item.content}
-
-                <Row gutter={[16, 16]}>
-                  <Col span={8}>
-                    <img
-                      width="100%"
-                      alt="logo"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                    />
-                  </Col>
-                  <Col span={8}>
-                    <img
-                      width="100%"
-                      alt="logo"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                    />
-                  </Col>
-                  <Col span={8}>
-                    <img
-                      width="100%"
-                      alt="logo"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                    />
-                  </Col>
-                  <Col span={8}>
-                    <img
-                      width="100%"
-                      alt="logo"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                    />
-                  </Col>
-                  <Col span={8}>
-                    <img
-                      width="100%"
-                      alt="logo"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                    />
-                  </Col>
-                  <Col span={8}>
-                    <img
-                      width="100%"
-                      alt="logo"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                    />
-                  </Col>
-                  <Col span={8}>
-                    <img
-                      width="100%"
-                      alt="logo"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                    />
-                  </Col>
-                  <Col span={8}>
-                    <img
-                      width="100%"
-                      alt="logo"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                    />
-                  </Col>
-                  <Col span={8}>
-                    <img
-                      width="100%"
-                      alt="logo"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                    />
-                  </Col>
-                </Row>
-              </List.Item>
-            )}
-          />
-        </Col>
-      </Row>
+        backgroundImageUrl={current.backgroundImageUrl}
+        title={current.label}></HeadBanner>
+      {React.createElement(current.component)}
     </div>
   );
 };
