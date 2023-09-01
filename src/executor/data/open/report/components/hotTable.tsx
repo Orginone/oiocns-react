f85fd5afe38962d7f66972933464dbd92c1e22bd
王佳鑫 <695997094@qq.com>
2023-09-01 16:05:10 +0800

    fix: 表报流程完善

--- src/executor/data/open/report/components/hotTable.tsx
+++ src/executor/data/open/report/components/hotTable.tsx
@@ -1,27 +1,26 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Tabs, Button } from 'antd';
-import { HotTable, HotColumn } from '@handsontable/react';
+import { Tabs } from 'antd';
+import { HotTable } from '@handsontable/react';
 import { HyperFormula } from 'hyperformula';
 import { textRenderer, registerRenderer } from 'handsontable/renderers';
 import { registerLanguageDictionary, zhCN } from 'handsontable/i18n';
-import Handsontable from 'Handsontable'
 registerLanguageDictionary(zhCN);
 import { registerAllModules } from 'handsontable/registry';
 registerAllModules();
 import 'handsontable/dist/handsontable.min.css';
-import { IReport } from '@/ts/core';
-import { selectEditor } from '../editor/select'
-import orgCtrl from '@/ts/controller';
+import { IForm } from '@/ts/core';
 interface IProps {
-  current: IReport;
+  current: IForm; // 获取样式
+  info: any // 数据
 }
 
-const HotTableView: React.FC<IProps> = ({ current }) => {
-  console.log(current,'current')
+// 数据还未获取,明细待完善
+const HotTableView: React.FC<IProps> = ({ current, info }) => {
   const [cells, setCells] = useState<any>([]);
   const [styleList, setStyleList] = useState<any>([]);
   const [classList, setClassList] = useState<any>([]);
   const [sheetIndex, setSheetIndex] = useState<string>('0');
+
   const hotRef: any = useRef(null);
   let sheetList = current.metadata?.rule ? JSON.parse(current.metadata?.rule) : [];
   let datas = sheetList[sheetIndex]?.data?.data || [[]];
@@ -32,112 +31,15 @@ const HotTableView: React.FC<IProps> = ({ current }) => {
 
   useEffect(() => {
     const hot = hotRef.current.hotInstance;
-    Handsontable.editors.registerEditor('SelectEditor', selectEditor)
     setCells(setting?.cells || []);
     setStyleList(setting?.styleList || []);
     setClassList(setting?.classList || []);
-
     hot.updateSettings({
       data: datas,
       cell: cells,
       mergeCells: mergeCells,
     });
   }, [sheetIndex]);
-
-  const setValidator = (item: any, rules: any) => {
-    // 设置单元格规则
-    hotRef.current.hotInstance.setCellMeta(
-      item.row,
-      item.col,
-      'validator',
-      function (value: any, callback: any) {
-        setTimeout(() => {
-          rules.forEach((items: any) => {
-            const reg = new RegExp(items);
-            if (reg.test(value)) {
-              callback(true);
-            } else {
-              callback(false);
-            }
-          });
-        }, 100);
-      },
-    );
-  };
-
-  const setEditor = (item: any) => {
-    // console.log(item,'1234')
-    let valueType: string = JSON.parse(item.prop.rule).widget
-    let newType: string = ''
-    switch (valueType) {
-      case 'select':
-      case 'dept':
-      case 'person':
-      case 'group':
-        newType = 'select'
-        setSelectOptions(item, valueType)
-        break
-      case 'myself':
-        newType = item.type
-        setData(item)
-        break
-      default:
-        newType = item.type
-        break
-    }
-
-    hotRef.current.hotInstance.setCellMeta(
-      item.row,
-      item.col,
-      'editor',
-      newType,
-    )
-  }
-
-  const setSelectOptions = (item: any, valueType: string) => {
-    //给下拉框插入数据
-    const belong = orgCtrl.targets.find(
-      (a) => a.id == current.metadata.belongId,
-    ) as any;
-    let arr: any = []
-    switch (valueType) {
-      case 'dept':
-        belong.departments?.map((xtarget: any) => {
-          arr.push(xtarget.name);
-        })
-        break
-      case 'person':
-        belong.members?.map((xtarget: any) => {
-          arr.push(xtarget.name);
-        })
-        break
-      case 'group':
-        belong.groups?.map((xtarget: any) => {
-          arr.push(xtarget.name);
-        })
-        break
-      default:
-        break
-    }
-    hotRef.current.hotInstance.setCellMeta(
-      item.row,
-      item.col,
-      'selectOptions',
-      arr,
-    )
-  }
-
-  const setData = (item: any) => {
-    const belong = orgCtrl.targets.find(
-      (a) => a.id == current.metadata.belongId,
-    ) as any;
-    
-    hotRef.current.hotInstance.setDataAtCell(
-      item.row,
-      item.col,
-      belong.user.name
-    )
-  }
 
   styleList?.forEach((item: any) => {
     hotRef.current.hotInstance.getCellMeta(item.row, item.col).renderer =
@@ -158,136 +60,12 @@ const HotTableView: React.FC<IProps> = ({ current }) => {
     );
   })
 
-  cells?.forEach((item: any) => {
-    //渲染单元格颜色
-    hotRef.current.hotInstance.getCellMeta(item.row, item.col).renderer =
-      'customStylesRenderer';
-
-    // 更新特性rules 但单元格只有只读属性 readOnly
-    current.attributes.forEach((items: any) => {
-      if (item.prop.propId === items.propId) {
-        item.prop = items;
-        let newRule = JSON.parse(item.prop.rule);
-        if (newRule) {
-          if (newRule.widget) {
-            setEditor(item)
-          }
-          for (var key in newRule) {
-            if (key === 'rules' && newRule['rules'].length > 0) {
-              setValidator(item, newRule['rules']);
-            } else if (key === 'min' || key === 'max') {
-              hotRef.current.hotInstance.setCellMeta(
-                item.row,
-                item.col,
-                'validator',
-                function (value: any, callback: any) {
-                  setTimeout(() => {
-                    if (value >= newRule['min'] && value <= newRule['max']) {
-                      callback(true);
-                    } else {
-                      callback(false);
-                    }
-                  }, 100);
-                },
-              );
-            } else {
-              hotRef.current.hotInstance.setCellMeta(
-                item.row,
-                item.col,
-                key,
-                newRule[key],
-              );
-            }
-          }
-        }
-      }
-    });
-  })
-
-  const saveData = () => {
-    const reportData = hotRef.current.hotInstance.getData()
-    const belong = orgCtrl.targets.find(
-      (a) => a.id == current.metadata.belongId,
-    ) as any;
-    let localData: any = []
-
-    cells?.forEach((item: any) => {
-      let json = { col: item.col, row: item.row, id: item.prop.propId, data: '', type: '' }
-      let newData: any
-      let items: any
-      let valueType: string = JSON.parse(item.prop.rule).widget
-      switch (valueType) {
-        case 'select':
-          json.type = 'select'
-          break
-        case 'dept':
-          json.type = 'dept'
-          newData = reportData[item.row][item.col]
-          items = belong.departments.find((it: any) => it.name === newData)
-          json.data = items?.id
-          break
-        case 'person':
-          json.type = 'person'
-          newData = reportData[item.row][item.col]
-          items = belong.members.find((it: any) => it.name === newData)
-          json.data = items?.id
-          break
-        case 'group':
-          json.type = 'group'
-          newData = reportData[item.row][item.col]
-          items = belong.groups.find((it: any) => it.name === newData)
-          json.data = items?.id
-          break
-        case 'myself':
-          json.type = 'myself'
-          json.data = belong.user.userId
-          break
-        default:
-          json.type = 'text'
-          json.data = reportData[item.row][item.col]
-          break
-      }
-      localData.push(json)
-    })
-    console.log(localData,'1234')
-  }
-
   const onChange = (key: string) => {
     setSheetIndex(key);
   };
 
   const hyperformulaInstance = HyperFormula.buildEmpty({
     licenseKey: 'internal-use-in-handsontable',
-  });
-
-  const afterChange = (change: any, source: any) => {
-    // 修改后
-    let arr: any = [];
-    if (change && change.length > 0) {
-      change.forEach((item: any) => {
-        let json1 = { row: item[0], col: item[1], val: item[3] };
-        arr.push(json1);
-      });
-    }
-    // rulesData = arr
-  };
-
-  const afterSetCellMeta = (row: Number, col: Number, key: string, val: boolean) => {
-    // console.log(row, col, key, val, 'row, col, key, val')
-    if (key != 'hidden' && key != 'spanned') {
-      // let json = {row:row,col:col,key:key,val:val}
-      // cellMeta.push(json)
-    }
-  };
-
-  const afterUpdateSettings = (change: any) => {
-    console.log(change, 'change');
-  };
-
-  registerRenderer('customStylesRenderer', (hotInstance: any, TD: any, ...rest) => {
-    //渲染特性背景色
-    textRenderer(hotInstance, TD, ...rest);
-    TD.style.background = '#e1f3d8';
   });
 
   registerRenderer('cellStylesRenderer', (hotInstance: any, TD: any, ...rest) => {
@@ -302,19 +80,12 @@ const HotTableView: React.FC<IProps> = ({ current }) => {
     }
   });
 
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <div>
-        <Button
-          type="primary"
-          shape="round"
-          size="small"
-          onClick={() => {
-            saveData()
-          }}>
-          提交
-        </Button>
-      </div>
       <HotTable
         ref={hotRef}
         formulas={{
@@ -322,10 +93,11 @@ const HotTableView: React.FC<IProps> = ({ current }) => {
         }}
         minCols={8}
         minRows={60}
+        readOnly={true}
         rowHeaders={true}
         colHeaders={true}
         dropdownMenu={true}
-        height="760px"
+        height="670px"
         language={zhCN.languageCode}
         stretchH="all"
         manualColumnResize={true}
@@ -334,14 +106,8 @@ const HotTableView: React.FC<IProps> = ({ current }) => {
         autoRowSize={autoRow}
         multiColumnSorting={true}
         outsideClickDeselects={false}
-        licenseKey="non-commercial-and-evaluation" // for non-commercial use only
-        afterChange={afterChange}
-        afterUpdateSettings={afterUpdateSettings}
-        afterSetCellMeta={afterSetCellMeta}
-      >
-        {/* @ts-ignore Element inherits some props. It's hard to type it. */}
-          {/* <SelectRenderer hot-renderer /> */}
-      </HotTable>
+        licenseKey="non-commercial-and-evaluation" // for non-commercial use only 
+      />
       <div>
         <Tabs
           tabPosition={'bottom'}
