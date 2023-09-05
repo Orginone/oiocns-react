1f4248cb20c67c064aed731a261729740c8552e6
jiangyang <1140938202@qq.com>
2023-09-05 14:54:02 +0800

    add:增加弹窗

--- src/layouts/Basic/navbar.tsx
+++ src/layouts/Basic/navbar.tsx
@@ -1,20 +1,20 @@
 import { useEffect, useState } from 'react';
-import { Badge, Drawer, Layout, List, Space, Tabs, Tag } from 'antd';
+import { Badge, Layout, Space, Popover, Row, Col } from 'antd';
 import { msgChatNotify } from '@/ts/core';
 import orgCtrl from '@/ts/controller';
 import styles from './index.module.less';
+import navStyles from './navbar.module.less';
 import { Link } from 'react-router-dom';
 import EntityIcon from '@/components/Common/GlobalComps/entityIcon';
 import OrgIcons from '@/components/Common/GlobalComps/orgIcons';
+import QrCode from 'qrcode.react';
 import React from 'react';
-import { kernel, model, schema } from '@/ts/base';
-import { showChatTime } from '@/utils/tools';
-
+import { BsFillPaletteFill } from 'react-icons/bs';
+import { AiFillHome, AiFillCreditCard, AiOutlineSecurityScan } from 'react-icons/ai';
+import { ImExit } from 'react-icons/im';
 const Navbar: React.FC = () => {
   const [workCount, setWorkCount] = useState(0);
   const [msgCount, setMsgCount] = useState(0);
-  const [online, setOnline] = useState(0);
-  const [onlineVisible, setOnlineVisible] = useState(false);
   useEffect(() => {
     const id = msgChatNotify.subscribe(() => {
       let noReadCount = 0;
@@ -25,9 +25,6 @@ const Navbar: React.FC = () => {
     });
     const workId = orgCtrl.work.notity.subscribe(async () => {
       setWorkCount(orgCtrl.work.todos.length);
-    });
-    kernel.onlineNotity.subscribe(() => {
-      setOnline(kernel.onlineIds.length);
     });
     return () => {
       msgChatNotify.unsubscribe(id);
@@ -90,21 +87,79 @@ const Navbar: React.FC = () => {
       </Link>
     );
   };
-
+  console.log('orgCtrl', orgCtrl.user);
+  const content = (
+    <div style={{ width: '200px' }}>
+      <div className={navStyles['nav-head']}>
+        <div className={navStyles['nav-left']}>
+          <Col>
+            <EntityIcon entityId={orgCtrl.user.id} size={45} />
+          </Col>
+          <Col className={navStyles['left-box']}>
+            <p>{orgCtrl.user.name}</p>
+            <div className={navStyles['nav-introduction']}>
+              {orgCtrl.user.information}
+            </div>
+          </Col>
+        </div>
+        <QrCode
+          level="H"
+          size={40}
+          fgColor={'#204040'}
+          value={`${location.origin}/${orgCtrl.user.id}`}
+          imageSettings={{
+            src: '',
+            width: 30,
+            height: 30,
+            excavate: true,
+          }}
+        />
+      </div>
+      <Row gutter={16} align="middle" className={navStyles['row-style']}>
+        <AiOutlineSecurityScan size={16} color="#888" />
+        <div>账号与安全</div>
+      </Row>
+      <Row gutter={16} align="middle" className={navStyles['row-style']}>
+        <AiFillCreditCard size={16} color="#888" />
+        <div>卡包设置</div>
+      </Row>
+      <Row gutter={16} align="middle" className={navStyles['row-style']}>
+        <AiFillHome size={16} color="#888" />
+        <div>门户设置</div>
+      </Row>
+      <Row gutter={16} align="middle" className={navStyles['row-style']}>
+        <BsFillPaletteFill size={16} color="#888" />
+        <div>主题设置</div>
+      </Row>
+      <Link
+        to={'/passport/login'}
+        onClick={() => {
+          sessionStorage.clear();
+          location.reload();
+        }}>
+        <Row gutter={16} align="middle" className={navStyles['row-style']}>
+          <ImExit size={16} color="#888" />
+          <div>退出设置</div>
+        </Row>
+      </Link>
+    </div>
+  );
   return (
     <Layout.Sider className={styles.header} width={60}>
-      <div className="ogo-space-item" onClick={() => setOnlineVisible(!onlineVisible)}>
-        {online > 0 ? (
-          <Badge count={online} size="small" offset={[-15, 0]}>
+      <div className="ogo-space-item">
+        <Popover
+          content={content}
+          trigger="click"
+          placement="right"
+          animation="false"
+          overlayClassName={navStyles['popover-style']}>
+          <div>
             <EntityIcon entityId={orgCtrl.user.id} size={45} />
-          </Badge>
-        ) : (
-          <EntityIcon entityId={orgCtrl.user.id} size={45} />
-        )}
+          </div>
+        </Popover>
       </div>
       <Space direction="vertical" wrap align="center" size={25} className={styles.navbar}>
         {actions.map((item) => NavItem(item))}
-        {onlineVisible && <OnlineInfo onClose={() => setOnlineVisible(false)} />}
       </Space>
       <Link
         to={'/passport/login'}
@@ -115,90 +170,6 @@ const Navbar: React.FC = () => {
         <OrgIcons size={26} exit selected />
       </Link>
     </Layout.Sider>
-  );
-};
-
-const OnlineInfo: React.FC<{ onClose: () => void }> = ({ onClose }) => {
-  const [key, setKey] = useState('1');
-  const [onlines, setOnlines] = useState<model.OnlineInfo[]>([]);
-  useEffect(() => {
-    const id = kernel.onlineNotity.subscribe((key) => {
-      kernel.onlines().then((value) => {
-        setOnlines(value);
-        setKey(key);
-      });
-    });
-    return () => {
-      kernel.onlineNotity.unsubscribe(id);
-    };
-  }, []);
-
-  const loadOnlineInfo = (onlines: model.OnlineInfo[]) => {
-    return (
-      <List
-        itemLayout="horizontal"
-        dataSource={onlines.sort(
-          (a, b) => new Date(b.onlineTime).getTime() - new Date(a.onlineTime).getTime(),
-        )}
-        renderItem={(item) => <OnlineItem data={item} />}
-      />
-    );
-  };
-
-  return (
-    <Drawer open width={500} placement="right" onClose={() => onClose()}>
-      <Tabs
-        key={key}
-        centered
-        items={[
-          {
-            key: 'online_user',
-            label: `在线用户(${onlines.filter((i) => i.userId != '0').length})`,
-            children: loadOnlineInfo(onlines.filter((i) => i.userId != '0')),
-          },
-          {
-            key: 'online_connection',
-            label: `在线连接(${onlines.filter((i) => i.userId === '0').length})`,
-            children: loadOnlineInfo(onlines.filter((i) => i.userId == '0')),
-          },
-        ]}
-      />
-    </Drawer>
-  );
-};
-
-const OnlineItem: React.FC<{ data: model.OnlineInfo }> = ({ data }) => {
-  const [target, setTarget] = useState<schema.XEntity>();
-  useEffect(() => {
-    if (data.userId != '0') {
-      orgCtrl.user.findEntityAsync(data.userId).then((item) => {
-        if (item) {
-          setTarget(item);
-        }
-      });
-    }
-  }, []);
-  return (
-    <List.Item
-      style={{ cursor: 'pointer', padding: 6 }}
-      actions={[
-        <div key={data.connectionId} title={data.onlineTime}>
-          {showChatTime(data.userId === '0' ? data.onlineTime : data.authTime)}
-        </div>,
-      ]}>
-      <List.Item.Meta
-        title={
-          <>
-            <span style={{ marginRight: 10 }}>{target?.name || data.connectionId}</span>
-            <Tag color="green" title={'请求次数'}>
-              {data.requestCount}
-            </Tag>
-          </>
-        }
-        avatar={<>{target && <EntityIcon entity={target} size={42} />}</>}
-        description={`使用地址:${data.remoteAddr}`}
-      />
-    </List.Item>
   );
 };
 
--- src/pages/Chats/index.tsx
+++ src/pages/Chats/index.tsx
@@ -3,9 +3,16 @@ import Content from './content';
 import * as config from './config/menuOperate';
 import MainLayout from '@/components/MainLayout';
 import useMenuUpdate from '@/hooks/useMenuUpdate';
-import { Input } from 'antd';
+import { Input, Modal, Dropdown, MenuProps } from 'antd';
 import { ImSearch } from 'react-icons/im';
-import { IMsgChat, msgChatNotify } from '@/ts/core';
+import { IMsgChat, msgChatNotify, TargetType } from '@/ts/core';
+import { schema } from '@/ts/base';
+import { companyTypes } from '@/ts/core';
+import SearchCompany from '@/components/Common/SearchTarget';
+import CreateTeamModal from './createTeam';
+import orgCtrl from '@/ts/controller';
+import * as ai from 'react-icons/ai';
+import * as im from 'react-icons/im';
 
 const Setting: React.FC<any> = () => {
   const [filter, setFilter] = useState('');
@@ -13,10 +20,43 @@ const Setting: React.FC<any> = () => {
   const [isSupervise, setIsSupervise] = useState<boolean>(false); // 查看所有会话
   const [key, rootMenu, selectMenu, setSelectMenu] = useMenuUpdate(config.loadChatMenu);
 
+  const [showFormModal, setShowFormModal] = useState<boolean>(false); // 创建单位开关
+  const [isModalOpen, setIsModalOpen] = useState(false); // 添加好友的开关
+  const [modalTitle, setModalTitle] = useState<string>(''); // 创建单位开关
+  const [selectTargetType, setSelectTargetType] = useState<TargetType>(); // 选中的用户类型
+  const [selectTarget, setSelectTarget] = useState<schema.XTarget[]>([]); // 选中的用户
+  const [createTargetType, setCreateTargetType] = useState<TargetType[]>([]); // 选中的用户类型
   /**
    * @description: 是否展示超级管理权处理
    * @return {*}
    */
+  const items: MenuProps['items'] = [
+    {
+      label: '加好友',
+      key: '加好友',
+      icon: <ai.AiOutlineUserAdd style={{ marginRight: 10 }} />,
+    },
+    {
+      label: '建群组',
+      key: '建群组',
+      icon: <ai.AiOutlineTeam style={{ marginRight: 10 }} />,
+    },
+    {
+      label: '加群组',
+      key: '加群组',
+      icon: <ai.AiOutlineUsergroupAdd style={{ marginRight: 10 }} />,
+    },
+    {
+      label: '建单位',
+      key: '建单位',
+      icon: <im.ImOffice style={{ marginRight: 10 }} />,
+    },
+    {
+      label: '加单位',
+      key: '加单位',
+      icon: <im.ImTree style={{ marginRight: 10 }} />,
+    },
+  ];
   useEffect(() => {
     if (selectMenu?.company === undefined && isSupervise) {
       setIsSupervise(false);
@@ -24,6 +64,41 @@ const Setting: React.FC<any> = () => {
   }, [selectMenu?.company]);
 
   if (!selectMenu || !rootMenu) return <></>;
+  const handleItemClick: MenuProps['onClick'] = (item) => {
+    switch (item.key) {
+      case '加好友':
+        setModalTitle('添加好友');
+        setSelectTargetType(TargetType.Person);
+        setIsModalOpen(true);
+        break;
+      case '建群组':
+        setModalTitle('创建群组');
+        setCreateTargetType([TargetType.Cohort]);
+        setShowFormModal(true);
+        break;
+      case '加群组':
+        setModalTitle('添加群组');
+        setSelectTargetType(TargetType.Cohort);
+        setIsModalOpen(true);
+        break;
+      case '建单位':
+        setModalTitle('创建单位');
+        setCreateTargetType(companyTypes);
+        setShowFormModal(true);
+        break;
+      case '加单位':
+        setModalTitle('添加单位');
+        setSelectTargetType(TargetType.Company);
+        setIsModalOpen(true);
+        break;
+      default:
+        break;
+    }
+  };
+  const menuProps = {
+    items,
+    onClick: handleItemClick,
+  };
   return (
     <MainLayout
       menusHeight={'calc(100vh - 168px)'}
@@ -32,14 +107,21 @@ const Setting: React.FC<any> = () => {
         setSelectMenu(data);
       }}
       rightBar={
-        <Input
-          style={{ height: 30, fontSize: 15 }}
-          placeholder="搜索"
-          allowClear
-          prefix={<ImSearch />}
-          onChange={(e) => {
-            setFilter(e.target.value);
-          }}></Input>
+        <>
+          <Input
+            style={{ height: 30, fontSize: 15 }}
+            placeholder="搜索"
+            allowClear
+            prefix={<ImSearch />}
+            onChange={(e) => {
+              setFilter(e.target.value);
+            }}></Input>
+          <Dropdown menu={menuProps} trigger={['click']}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <ai.AiOutlinePlus style={{ marginRight: 10, fontSize: '24px' }} />
+            </div>
+          </Dropdown>
+        </>
       }
       onMenuClick={async (data, key) => {
         const chat = data.item as IMsgChat;
@@ -68,6 +150,46 @@ const Setting: React.FC<any> = () => {
         selectMenu={selectMenu}
         openDetail={openDetail}
         filter={filter}
+      />
+      {/* 添加用户 */}
+      {selectTargetType && (
+        <Modal
+          destroyOnClose
+          title={modalTitle}
+          okButtonProps={{ disabled: !selectTarget }}
+          open={isModalOpen}
+          onOk={async () => {
+            if (await orgCtrl.user.applyJoin(selectTarget)) {
+              setIsModalOpen(false);
+            }
+          }}
+          onCancel={() => {
+            setIsModalOpen(false);
+          }}
+          width={670}>
+          <SearchCompany
+            searchCallback={(persons: schema.XTarget[]) => {
+              setSelectTarget(persons);
+            }}
+            searchType={selectTargetType}
+          />
+        </Modal>
+      )}
+      {/* 创建用户 */}
+      <CreateTeamModal
+        title={modalTitle}
+        isEdit={false}
+        open={showFormModal}
+        current={orgCtrl.user}
+        typeNames={createTargetType}
+        handleCancel={() => {
+          setShowFormModal(false);
+        }}
+        handleOk={(item) => {
+          if (item) {
+            setShowFormModal(false);
+          }
+        }}
       />
     </MainLayout>
   );
