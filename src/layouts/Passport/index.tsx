f1e13563856e9ffc50fe15809af4e921244df751
lilming <3139457157@qq.com>
2023-09-19 15:20:12 +0800

    style(passport): 登录页面的响应式布局

--- src/layouts/Passport/index.tsx
+++ src/layouts/Passport/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { JSX } from 'react';
 import { renderRoutes } from 'react-router-config';
 
 import type { IRouteConfig } from 'typings/globelType';
 
 import cls from './index.module.less';
 const PassportLayout: React.FC<{ route: IRouteConfig }> = ({ route }) => {
+  const [text, setText] = React.useState<JSX.Element>(
+    <div>
+      这是一段示例文字
+      <br />
+      这是一段示例文字
+      <br />
+      这是一段示例文字
+      <br />
+      这是一段示例文字
+      <br />
+      这是一段示例文字
+    </div>,
+  );
   return (
     <div className={cls.contaner}>
-      <img className={cls.bg} src="/img/passport.png" alt="" />
+      <img className={cls.wave} src="/img/passport_bg.png" alt="" />
+      <div className={cls.img}>
+        <img src="/img/passport_img.png" alt="" />
+        {text}
+      </div>
       <div className={cls.box}>
         <div>{renderRoutes(route.routes)}</div>
       </div>
