19275034c97a28750958b7600a3eb16d31ec0b88
realVeer <realVeer@qkbyte.com>
2023-10-18 12:48:30 +0800

    fix: 添加网站图标

--- src/components/MainLayout/preview/index.tsx
+++ src/components/MainLayout/preview/index.tsx
@@ -1,6 +1,14 @@
 import ImageView from './image';
 import VideoView from './video';
-import { IEntity, IForm, ISession, ISysFileInfo, IWorkTask } from '@/ts/core';
+import {
+  IEntity,
+  IForm,
+  ISession,
+  ISysFileInfo,
+  ITarget,
+  IWorkTask,
+  TargetType,
+} from '@/ts/core';
 import { command, schema } from '@/ts/base';
 import React, { useEffect, useState } from 'react';
 import OfficeView from './office';
@@ -13,70 +21,78 @@ import WorkForm from '@/components/DataStandard/WorkForm';
 const officeExt = ['.md', '.pdf', '.xls', '.xlsx', '.doc', '.docx', '.ppt', '.pptx'];
 const videoExt = ['.mp4', '.avi', '.mov', '.mpg', '.swf', '.flv', '.mpeg'];
 
+type EntityType =
+  | IEntity<schema.XEntity>
+  | ISysFileInfo
+  | ISession
+  | IWorkTask
+  | IForm
+  | ITarget
+  | string
+  | undefined;
+
 interface IOpenProps {
   flag?: string;
-  entity:
-    | IEntity<schema.XEntity>
-    | ISysFileInfo
-    | ISession
-    | IWorkTask
-    | IForm
-    | string
-    | undefined;
+  entity: EntityType;
 }
+
+/** 文件预览 */
+const FilePreview: React.FC<{ file: ISysFileInfo }> = ({ file }) => {
+  const data = file.filedata;
+  if (data.contentType?.startsWith('image')) {
+    return <ImageView share={data} />;
+  }
+  if (data.contentType?.startsWith('video') || videoExt.includes(data.extension ?? '-')) {
+    return <VideoView share={data} />;
+  }
+  if (officeExt.includes(data.extension ?? '-')) {
+    return <OfficeView share={data} />;
+  }
+  return <EntityInfo entity={file} column={1} />;
+};
+
+/** 实体预览 */
 const EntityPreview: React.FC<IOpenProps> = (props: IOpenProps) => {
-  if (props.flag && props.flag.length > 0) {
-    const [entity, setEntity] = useState(props.entity);
-    useEffect(() => {
-      const id = command.subscribe((type, flag, ...args: any[]) => {
-        if (type != 'preview' || flag != props.flag) return;
-        if (args && args.length > 0) {
-          setEntity(args[0]);
-        } else {
-          setEntity(undefined);
-        }
-      });
-      return () => {
-        command.unsubscribe(id);
-      };
-    }, [props.flag]);
-    if (entity && typeof entity != 'string') {
-      if ('filedata' in entity) {
-        const data = entity.filedata;
-        if (data.contentType?.startsWith('image')) {
-          return <ImageView share={data} />;
-        }
-        if (
-          data.contentType?.startsWith('video') ||
-          videoExt.includes(data.extension ?? '-')
-        ) {
-          return <VideoView share={data} />;
-        }
-        if (officeExt.includes(data.extension ?? '-')) {
-          return <OfficeView share={data} />;
-        }
+  if (!(props.flag && props.flag.length > 0)) return <></>;
+  const [entity, setEntity] = useState<EntityType>(props.entity);
+  useEffect(() => {
+    const id = command.subscribe((type, flag, ...args: any[]) => {
+      if (type != 'preview' || flag != props.flag) return;
+      if (args && args.length > 0) {
+        setEntity(args[0]);
+      } else {
+        setEntity(undefined);
       }
-      if ('activity' in entity) {
-        return <SessionBody chat={entity} />;
-      }
-      if ('session' in entity) {
-        return <SessionBody chat={entity.session} store />;
-      }
-      if ('fields' in entity) {
-        return <WorkForm form={entity} />;
-      }
-      if ('taskdata' in entity) {
-        switch (entity.taskdata.taskType) {
-          case '事项':
-            return <TaskBody task={entity} />;
-          case '加用户':
-            return <JoinApply task={entity} />;
-          default:
-            return <></>;
-        }
-      }
-      return <EntityInfo entity={entity} column={1} />;
+    });
+    return () => {
+      command.unsubscribe(id);
+    };
+  }, [props.flag]);
+
+  if (entity && typeof entity != 'string') {
+    if ('filedata' in entity) {
+      return <FilePreview file={entity} />;
     }
+    if ('activity' in entity) {
+      return <SessionBody chat={entity} />;
+    }
+    if ('session' in entity && entity.typeName !== TargetType.Storage) {
+      return <SessionBody chat={entity.session} store />;
+    }
+    if ('fields' in entity) {
+      return <WorkForm form={entity} />;
+    }
+    if ('taskdata' in entity) {
+      switch (entity.taskdata.taskType) {
+        case '事项':
+          return <TaskBody task={entity} />;
+        case '加用户':
+          return <JoinApply task={entity} />;
+        default:
+          return <></>;
+      }
+    }
+    return <EntityInfo entity={entity} column={1} />;
   }
   return <></>;
 };
