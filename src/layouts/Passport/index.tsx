f4140f4c8428727cf4f056eb70931f319478466b
lilming <3139457157@qq.com>
2023-11-03 23:12:51 +0800

    fix(passport): 修改登录页面样式，基于原先的结构，替换更清晰图片，增加背景，轮播图使用antd组件

--- src/layouts/Passport/index.tsx
+++ src/layouts/Passport/index.tsx
@@ -1,86 +1,68 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { renderRoutes } from 'react-router-config';
 import { getResouces } from '@/config/location';
 import type { IRouteConfig } from 'typings/globelType';
 
 import cls from './index.module.less';
+import { Carousel } from 'antd';
 const passportLayout: React.FC<{ route: IRouteConfig }> = ({ route }) => {
-  const [index, setIndex] = useState(1);
   const resources = getResouces();
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setIndex((prevIndex) => {
-        let nextIndex = prevIndex + 1;
-        if (nextIndex === resources.passport.length + 1) {
-          nextIndex = 1;
-        }
-        return nextIndex;
-      });
-    }, 5000);
-    return () => clearInterval(timer);
-  }, [index]);
-
-  const choosePic = (e: React.MouseEvent<HTMLDivElement>) => {
-    const chooseIndex = parseInt(e.currentTarget.id.charAt(3));
-    setIndex(chooseIndex);
-  };
   return (
     <div className={cls.container}>
       <div className={cls.slider}>
-        <img
-          style={{ height: '100vh' }}
-          src={`/img/${resources.location}/passport/page${index}.png`}
-          alt=""
-        />
-        {resources.passport.map((i) => {
-          return (
-            <input
-              key={`btn${i}`}
-              id={`btn${i}`}
-              className={cls[`btn${i}`]}
-              type="radio"
-              onClick={choosePic}
-            />
-          );
-        })}
-        <div className={cls.count}>
-          {resources.passport.map((i) => {
+        <Carousel autoplay autoplaySpeed={4000} dots={{ className: 'passportDots' }}>
+          {resources.passport.map((index) => {
             return (
-              <div key={`countItem${i}`} className={cls.countItem}>
-                <label
-                  style={{
-                    background: index === i ? '#ffffff' : '#eff5ff',
-                    width: index === i ? '32px' : '',
-                  }}
-                  htmlFor={`btn${i}`}></label>
+              <div key={index}>
+                <img
+                  key={index}
+                  className={cls.sliderImg}
+                  src={`/img/${resources.location}/passport/page${index}.png`}
+                  alt=""
+                />
               </div>
             );
           })}
-        </div>
+        </Carousel>
       </div>
-      <div className={cls.content}>
-        <div></div>
-        <div style={{ width: 350 }}>{renderRoutes(route.routes)}</div>
-        <div className={cls.copyright}>
-          {resources.unitName.length > 0 && (
-            <>
-              <div>
-                主办单位：
-                <a href={resources.unitPage} target="_blank" rel="noreferrer">
-                  {resources.unitName}
-                </a>
-              </div>
-              <div>
-                &nbsp; 技术支持：
-                <a href="https://ocia.orginone.cn" target="_blank" rel="noreferrer">
-                  资产云开放协同创新中心
-                </a>
-              </div>
-            </>
-          )}
-          <a href="https://orginone.cn" target="_blank" rel="noreferrer">
-            Powered by Orginone
+      <div className={cls.background}>
+        <img alt={''} src={`/img/${resources.location}/passport/background.png`} />
+      </div>
+      <div className={cls.login}>
+        <div className={cls.loginBox}>{renderRoutes(route.routes)}</div>
+      </div>
+      <div className={cls.copyright}>
+        <div className={cls.copyrightEn}>
+          <a
+            href="https://orginone.cn"
+            target="_blank"
+            style={{ color: '#8a8a8a' }}
+            rel="noopener noreferrer">
+            Powered by Orginone{' '}
           </a>
+        </div>
+        <div className={cls.copyrightZh}>
+          <div>
+            {resources.unitName !== '' ? '主办单位：' : ''}
+            <a
+              href={resources.unitPage}
+              target="_blank"
+              style={{ color: '#8a8a8a' }}
+              rel="noopener noreferrer">
+              {resources.unitName}
+            </a>
+            &nbsp;
+          </div>
+          <div>
+            技术支持：
+            <a
+              href="https://assetcloud.org.cn"
+              target="_blank"
+              style={{ color: '#8a8a8a' }}
+              rel="noopener noreferrer">
+              资产云开放协同创新中心
+            </a>
+          </div>
         </div>
       </div>
     </div>
