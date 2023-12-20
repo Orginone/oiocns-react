5c0393d1f79af2a2c64b00d10d4576cd55f5105e
王佳鑫 <695997094@qq.com>
2023-12-20 17:49:38 +0800

    fix: 合并代码

--- src/pages/Chats/index.tsx
+++ src/pages/Chats/index.tsx
@@ -4,10 +4,7 @@ import MainLayout from '@/components/MainLayout';
 import useMenuUpdate from '@/hooks/useMenuUpdate';
 import { Input } from 'antd';
 import { ImSearch } from 'react-icons/im';
-<<<<<<< HEAD
-=======
 import OrgIcons from '@/components/Common/GlobalComps/orgIcons';
->>>>>>> fba29b5bf6084e336c5d8988fd86ed97535f91a3
 
 const Setting: React.FC<any> = () => {
   const [filter, setFilter] = useState('');
@@ -25,10 +22,7 @@ const Setting: React.FC<any> = () => {
   return (
     <MainLayout
       rightShow
-<<<<<<< HEAD
-=======
       leftShow={false}
->>>>>>> fba29b5bf6084e336c5d8988fd86ed97535f91a3
       previewFlag={'chat'}
       selectMenu={selectMenu}
       onSelect={async (data) => {
