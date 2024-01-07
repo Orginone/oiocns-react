15a5a3e2a85c04dd2a3c65ff9007ac0c8a9404de
15168347908 <296778233@qq.com>
2024-01-07 22:18:31 +0800

    fix: 迁移相关代码修改

--- src/ts/core/work/executor/acquire.ts
+++ src/ts/core/work/executor/acquire.ts
@@ -38,12 +38,8 @@ export class Acquire extends Executor {
    * @returns 总进度
    */
   private async getTotalCount(work: IWork): Promise<number> {
-    let count = 0;
-    for (const form of this.task.instanceData?.node.detailForms ?? []) {
-      const things = await this.loadThing(1, 0, form.id, work);
-      count += things.totalCount;
-    }
-    return count;
+    const things = await this.loadThing(1, 0, work);
+    return things.totalCount;
   }
   /**
    * 迁移数据
@@ -55,18 +51,16 @@ export class Acquire extends Executor {
     for (const company of orgCtrl.user.companys) {
       if (company.id == this.task.taskdata.applyId) {
         const thingColl = company.resource.thingColl;
-        for (const form of this.task.instanceData?.node.detailForms ?? []) {
-          let take = 1000;
-          let skip = 0;
-          while (true) {
-            const things = await this.loadThing(take, skip, form.id, work);
-            await thingColl.replaceMany(things.data);
-            skip += things.data.length;
-            loaded += things.data.length;
-            onProgress(loaded);
-            if (things.data.length == 0) {
-              break;
-            }
+        let take = 500;
+        let skip = 0;
+        while (true) {
+          const things = await this.loadThing(take, skip, work, false);
+          await thingColl.replaceMany(things.data);
+          skip += things.data.length;
+          loaded += things.data.length;
+          onProgress(loaded);
+          if (things.data.length == 0) {
+            break;
           }
         }
       }
@@ -76,43 +70,51 @@ export class Acquire extends Executor {
    * 迁移文件
    */
   private async transferFile(work: IWork): Promise<void> {
-    const group = work.application.directory.target;
-    const groupDirs = await group.directory.standard.loadDirectorys();
-    const groupFileDir = groupDirs.find((item) => item.name.includes('数据附件'));
-    if (!groupFileDir) {
-      return;
-    }
-    const childDirs = await groupFileDir.standard.loadDirectorys();
-    const targetDir = childDirs.find((item) => item.code == group.space.code);
-    if (!targetDir) {
-      return;
-    }
-    const spaceDirs = await group.space.directory.standard.loadDirectorys();
-    let fileDir = spaceDirs.find((item) => item.name.includes('数据附件'));
-    if (!fileDir) {
-      const resultDir = await group.space.directory.create({
-        name: '数据附件',
-        code: 'dataFile',
-        typeName: '目录',
-        directoryId: group.space.id,
-      } as schema.XDirectory);
-      if (resultDir) {
-        fileDir = new Directory(resultDir, group.space, group.space.directory);
-      }
-    }
-    if (fileDir) {
-      const ready = (await fileDir.loadFiles()).map((item) => item.code.split('/')[1]);
-      const files = (await targetDir.loadFiles()).filter((item) => {
-        return !ready.includes(item.code.split('/')[1]);
-      });
-      if (files.length > 0) {
-        command.emitter('executor', 'taskList', fileDir);
-      }
-      for (const file of files) {
-        const response = await fetch(file.shareInfo().shareLink ?? '');
-        if (response.ok) {
-          const blob = await response.blob();
-          fileDir.createFile(new File([blob], file.name, { type: blob.type }));
+    const directoryColl = work.application.directory.target.resource.fileDirectoryColl;
+    for (const company of orgCtrl.user.companys) {
+      if (company.id == this.task.taskdata.applyId) {
+        const directories = await directoryColl.loadSpace({
+          options: {
+            match: {
+              code: company.metadata.code,
+            },
+          },
+        });
+        if (directories.length > 0) {
+          for (const item of directories) {
+            const groupDir = new Directory(item, company);
+            const children = await company.directory.standard.loadDirectorys();
+            let fileDir = children.find((item) => item.name.includes('数据附件'));
+            if (!fileDir) {
+              const resultDir = await company.directory.create({
+                name: '数据附件',
+                code: 'dataFile',
+                typeName: '目录',
+                directoryId: company.id,
+              } as schema.XDirectory);
+              if (resultDir) {
+                fileDir = new Directory(resultDir, company, company.directory);
+              }
+            }
+            if (fileDir) {
+              const ready = (await fileDir.loadFiles()).map(
+                (item) => item.code.split('/')[1],
+              );
+              const files = (await groupDir.loadFiles()).filter((item) => {
+                return !ready.includes(item.code.split('/')[1]);
+              });
+              if (files.length > 0) {
+                command.emitter('executor', 'taskList', fileDir);
+              }
+              for (const file of files) {
+                const response = await fetch(file.shareInfo().shareLink ?? '');
+                if (response.ok) {
+                  const blob = await response.blob();
+                  fileDir.createFile(new File([blob], file.name, { type: blob.type }));
+                }
+              }
+            }
+          }
         }
       }
     }
@@ -125,12 +127,17 @@ export class Acquire extends Executor {
    * @param work 办事
    * @returns 物信息
    */
-  private async loadThing(take: number, skip: number, form: string, work: IWork) {
+  private async loadThing(
+    take: number,
+    skip: number,
+    work: IWork,
+    requireTotalCount = true,
+  ) {
     const loadOptions = {
       take: take,
       skip: skip,
-      requireTotalCount: true,
-      userData: [`F${form}`],
+      requireTotalCount: requireTotalCount,
+      userData: [],
       filter: ['belongId', '=', this.task.taskdata.applyId],
     };
     return await kernel.loadThing(
